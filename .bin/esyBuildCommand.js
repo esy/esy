be49ca0e7363002b36abc96275368519d46af675
Andrey Popp <8mayday@gmail.com>
2016-12-04 20:43:32 +0300

    Move findlib.conf generation to esy build command

--- .bin/esyBuildCommand.js
+++ .bin/esyBuildCommand.js
@@ -1,5 +1,17 @@
-const path = require('path');
 const childProcess = require('child_process');
+const path = require('path');
+
+const ESY_SANDBOX_REF = '$(ESY_SANDBOX)';
+
+function installDir(pkgName, ...args) {
+  return path.join(
+    '$(ESY_SANDBOX)', '_install', 'node_modules', pkgName, ...args);
+}
+
+function buildDir(pkgName, ...args) {
+  return path.join(
+    '$(ESY_SANDBOX)', '_build', 'node_modules', pkgName, ...args);
+}
 
 function buildCommand(curDir, env, args) {
 
@@ -12,14 +24,62 @@ function buildCommand(curDir, env, args) {
     return envMap;
   }, new Map());
 
-  let rules = env
-    .map(pkg => generateMakeRule(builtIns, envMap, pkg))
-    .filter(rule => rule != null);
-  let renderedRules = rules
-    .map(rule => renderMakeRule(rule))
-    .join('\n\n');
+  let rules = [];
 
-  console.log(renderedRules);
+  let prelude = [
+    {
+      type: 'define',
+      name: 'ESY_SANDBOX',
+      value: '$(PWD)',
+      assignment: '?=',
+    },
+  ];
+
+  env.forEach(pkg => {
+    let info = pkg.packageJson;
+    let dependencies = collectTransitiveDependencies(envMap, info);
+
+    let findlibPath = dependencies
+      .map(dep => installDir(dep, 'lib'))
+      .join(':');
+
+    prelude.push({
+      type: 'define',
+      multiline: true,
+      name: `${info.name}__FINDLIB_CONF`,
+      value: `
+path = "$(shell ocamlfind printconf path):${findlibPath}"
+destdir = "${installDir(info.name, 'lib')}"
+      `.trim(),
+    });
+
+    rules.push({
+      type: 'rule',
+      name: null,
+      target: `${info.name}__findlib.conf`,
+      dependencies: [buildDir(info.name, 'findlib.conf')],
+      env: {},
+      command: null,
+    });
+    rules.push({
+      type: 'rule',
+      name: null,
+      target: buildDir(info.name, 'findlib.conf'),
+      dependencies: [],
+      env: {},
+      export: [`${info.name}__FINDLIB_CONF`],
+      command: `
+mkdir -p $(@D)
+echo "$${info.name}__FINDLIB_CONF" > $(@);
+      `.trim(),
+    });
+  });
+
+  rules = rules.concat(env
+    .map(pkg => generateMakeRule(builtIns, envMap, pkg))
+    .filter(rule => rule != null));
+
+  console.log(renderMake(prelude.concat(rules)));
 }
 
 function generateMakeRule(builtIns, envMap, pkg) {
@@ -31,26 +91,34 @@ function generateMakeRule(builtIns, envMap, pkg) {
     }));
   }
 
-  if (pkg.packageJson.pjc && pkg.packageJson.pjc.build) {
+  let info = pkg.packageJson;
+
+  if (info.pjc && info.pjc.build) {
+    let dependencies = [
+      `${info.name}__findlib.conf`
+    ];
+    if (info.dependencies) {
+      dependencies = dependencies.concat(Object.keys(info.dependencies));
+    }
     return {
-      name: ` *** Build ${pkg.packageJson.name} ***`,
-      target: pkg.packageJson.name,
-      dependencies: pkg.packageJson.dependencies != null
-        ? Object.keys(pkg.packageJson.dependencies)
-        : [],
+      type: 'rule',
+      name: ` *** Build ${info.name} ***`,
+      target: info.name,
+      dependencies: dependencies,
       env: []
         .concat(envToEnvList(builtIns))
         .concat(getBuildEnv(envMap, pkg))
         .concat(envToEnvList(pkg)),
-      command: pkg.packageJson.pjc.build,
+      command: info.pjc.build,
     };
   } else {
     // TODO: Returning an empty rule. Is that really what we want here?
     return {
-      name: ` *** Build ${pkg.packageJson.name} ***`,
-      target: pkg.packageJson.name,
-      dependencies: pkg.packageJson.dependencies != null
-        ? Object.keys(pkg.packageJson.dependencies)
+      type: 'rule',
+      name: ` *** Build ${info.name} ***`,
+      target: info.name,
+      dependencies: info.dependencies != null
+        ? Object.keys(info.dependencies)
         : [],
       command: null,
     };
@@ -58,7 +126,7 @@ function generateMakeRule(builtIns, envMap, pkg) {
 }
 
 function getPkgEnv(envMap, pkg, current = false) {
-  let prefix = current ? 'cur' : (pkg.packageJson.name + '_')
+  let prefix = current ? 'cur' : (pkg.packageJson.name + '_');
   let info = pkg.packageJson;
   return [
     {
@@ -77,14 +145,6 @@ function getPkgEnv(envMap, pkg, current = false) {
       name: `${prefix}_depends`,
       value: info.dependencies != null
         ? `"${Object.keys(info.dependencies).join(' ')}"`
-        : null,
-    },
-    {
-      name: `${prefix}_depends_install_closed`,
-      value: info.dependencies != null
-      ? `"${collectTransitiveDependencies(envMap, info)
-            .map(dep => `$_install_tree/node_modules/${dep}/lib`)
-            .join(':')}"`
         : null,
     },
     {
@@ -134,41 +194,39 @@ function getPkgEnv(envMap, pkg, current = false) {
   ];
 }
 
-function getBuildEnv(envMap, pkg, onlyMeta = false) {
+function getBuildEnv(envMap, pkg) {
   let info = pkg.packageJson;
   let name = info.name;
   let dependencies = info.dependencies != null
     ? Object.keys(info.dependencies)
     : [];
   let pkgEnv = [];
-  if (!onlyMeta) {
-    pkgEnv = pkgEnv.concat([
-      {
-        name: 'FINDLIB_CONF',
-        value: null, // TODO
-      },
-      {
-        name: 'sandbox',
-        value: '$ESY__SANDBOX',
-      },
-      {
-        name: '_install_tree',
-        value: '$sandbox/_install',
-      },
-      {
-        name: '_build_tree',
-        value: '$sandbox/_build',
-      },
-    ]);
 
-    pkgEnv = pkgEnv.concat(getPkgEnv(envMap, pkg, true));
-  }
+  pkgEnv = pkgEnv.concat([
+    {
+      name: 'sandbox',
+      value: '$ESY__SANDBOX',
+    },
+    {
+      name: '_install_tree',
+      value: '$sandbox/_install',
+    },
+    {
+      name: '_build_tree',
+      value: '$sandbox/_build',
+    },
+    {
+      name: 'OCAMLFIND_CONF',
+      value: `$_build_tree/node_modules/${name}/findlib.conf`,
+    },
+  ]);
 
+  pkgEnv = pkgEnv.concat(getPkgEnv(envMap, pkg, true));
   pkgEnv = pkgEnv.concat(getPkgEnv(envMap, pkg, false));
 
-  if (!onlyMeta && dependencies.length > 0) {
+  if (dependencies.length > 0) {
     pkgEnv = pkgEnv.concat(
-      ...dependencies.map(dep => getBuildEnv(envMap, envMap.get(dep), true))
+      ...dependencies.map(dep => getPkgEnv(envMap, envMap.get(dep), false))
     );
     let depPath = dependencies.map(dep => `$_install_tree/${dep}/bin`).join(':');
     let depManPath = dependencies.map(dep => `$_install_tree/${dep}/man`).join(':');
@@ -183,6 +241,7 @@ function getBuildEnv(envMap, pkg, onlyMeta = false) {
       }
     ]);
   }
+
   return pkgEnv;
 }
 
@@ -202,27 +261,69 @@ function collectTransitiveDependencies(envMap, info) {
   }
 }
 
-function renderMakeRule(rule) {
-  let header = `${rule.target}: ${rule.dependencies.join(' ')}`;
-  if (rule.command != null) {
-    let recipe = renderMakeRuleCommand(rule.env, rule.command);
-    recipe = recipe.replace(/\$/g, '\$\$\$');
-    return `${header}\n\t@echo '${rule.name}'\n\t@${recipe}`;
+function renderMake(items) {
+  return items
+    .map(item => {
+      if (item.type === 'rule') {
+        return renderMakeRule(item);
+      } else if (item.type === 'define') {
+        return renderMakeDefine(item);
+      } else {
+        throw new Error('Unknown make item:' + JSON.stringify(item));
+      }
+    })
+    .join('\n\n');
+}
+
+function renderMakeDefine({name, value, multiline, assignment = '='}) {
+  if (multiline) {
+    return `
+define ${name}
+${value}
+endef
+    `.trim();
   } else {
-    return header;
+    return `${name} ${assignment} ${value}`;
   }
 }
 
-function renderMakeRuleCommand(env, command) {
+function renderMakeRule(rule) {
+  let header = `${rule.target}: ${rule.dependencies.join(' ')}`;
+
+  let prelude = '';
+  if (rule.export) {
+    rule.export.forEach(name => {
+      prelude = prelude + `export ${name}\n`;
+    });
+  }
+
+  if (rule.command != null) {
+    let recipe = escapeEnvVar(renderMakeRuleCommand(rule));
+    if (rule.name != null) {
+      return `${prelude}${header}\n\t@echo '${rule.name}'\n${recipe}`;
+    } else {
+      return `${prelude}${header}\n${recipe}`;
+    }
+  } else {
+    return prelude + header;
+  }
+}
+
+function renderMakeRuleCommand({env, command}) {
+  command = command.split('\n').map(line => `\t${line}`).join('\n');
   if (env.length > 0) {
     let renderedEnv = env
       .filter(env => env.value != null)
-      .map(env => `export ${env.name}=${env.value}; \\`)
-      .join('\n\t');
-    return `${renderedEnv}\n\t${command}`;
+      .map(env => `\texport ${env.name}=${env.value}; \\`)
+      .join('\n');
+    return `${renderedEnv}\n${command}`;
   } else {
     return command;
   }
+}
+
+function escapeEnvVar(command) {
+  return command.replace(/\$([^\(])/g, '$$$$$1');
 }
 
 module.exports = buildCommand;
