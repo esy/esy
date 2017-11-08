e4b1c33099e27f33ab428b3bd2bcf304b7ffad71
Andrey Popp <8mayday@gmail.com>
2017-11-08 13:45:52 +0300

    Refactor release integration tests to run in parallel

--- __tests__/release/fixtures/minimal/test.js
+++ __tests__/release/fixtures/minimal/test.js
@@ -6,44 +6,36 @@ jest.setTimeout(200000);
 
 import * as os from 'os';
 import * as path from 'path';
-import {
-  readDirectory,
-  cleanUp,
-  esyBin,
-  esyRoot,
-  runIn,
-  packAndNpmInstallGlobal,
-  initFixtureSync,
-} from '../../utils';
+import {readDirectory, cleanUp, initFixtureSync} from '../../utils';
 
 export default function initTest(params: {releaseType: string}) {
   const fixture = initFixtureSync(__dirname);
   const description = `esy release ${params.releaseType}: ${fixture.description}`;
 
-  const releaseDir =
+  const releaseTag =
     params.releaseType === 'bin' ? `bin-${os.platform()}` : params.releaseType;
 
   const test = async () => {
-    await runIn(fixture.project, 'npm', 'install');
-    expect(
-      await runIn(
-        fixture.project,
-        esyBin,
-        'release',
-        params.releaseType,
-        '--esy-version-for-dev-release',
-        esyRoot,
-      ),
-    ).toMatchSnapshot('release stdout');
+    await fixture.npm(['install'], {cwd: fixture.project});
 
-    expect(await readDirectory(fixture.project, '_release')).toMatchSnapshot('release');
+    const releaseStdout = await fixture.esyRelease(params.releaseType);
+    expect(releaseStdout).toMatchSnapshot('release stdout');
 
-    expect(
-      await packAndNpmInstallGlobal(fixture, '_release', releaseDir),
-    ).toMatchSnapshot('installation stdout');
+    const releaseDir = await readDirectory(path.join(fixture.project, '_release'), {
+      filter,
+    });
+    expect(releaseDir).toMatchSnapshot('release');
 
-    expect(await readDirectory(fixture.npmPrefix)).toMatchSnapshot('installation');
+    const installationStdout = await fixture.npmPackAndInstall(['_release', releaseTag]);
+    expect(installationStdout).toMatchSnapshot('installation stdout');
+
+    const installationDir = await readDirectory(fixture.npmPrefix, {filter});
+    expect(installationDir).toMatchSnapshot('installation');
   };
 
   return {cleanUp, description, test};
 }
+
+const filter = filename => {
+  return filename !== '<root>/lib/node_modules/minimal/_esy';
+};
