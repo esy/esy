bde2d850432beb7f21b910ce14a3848249a440d1
Andrey Popp <8mayday@gmail.com>
2017-11-09 18:30:13 +0300

    Add e2e test for symbplic link workflow

---
+++ __tests__/symlink-workflow-test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+source ./testlib.sh
+
+initFixture symlink-workflow
+
+pushd ./app
+
+run esy install
+run esy add link:../dep
+
+# just for debug
+cat package.json
+ls -la node_modules
+
+run esy build
+
+assert_stdout 'esy dep' 'HELLO'
+
+info "modify dep sources"
+cat <<EOF > ../dep/dep
+#!/bin/bash
+
+echo HELLO_MODIFIED
+EOF
+
+run esy build
+assert_stdout 'esy dep' 'HELLO_MODIFIED'
---
+++ __tests__/testlib.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+set -u
+
+# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  SCRIPTDIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$SCRIPTDIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPTDIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+ESYCOMMAND="$SCRIPTDIR/../bin/esy"
+
+initFixture () {
+  local root
+  local name
+  local project
+
+  name="$1"
+  root=$(mktemp -d)
+  project="$root/project"
+
+  cp -r "fixtures/$name" "$project"
+
+  esy () {
+    "$ESYCOMMAND" "$@"
+  }
+
+  pushd "$project"
+}
+
+info () {
+  >&2 echo "$1"
+}
+
+run () {
+  echo "RUNNING: " "$@"
+  "$@"
+}
+
+failwith () {
+  >&2 echo "ERROR: $1"
+  exit 1
+}
+
+assert_stdout () {
+  local command="$1"
+  local expected="$2"
+  local actual
+  echo "RUNNING: $command"
+  actual=$($command)
+  if [ ! $? -eq 0 ]; then
+    failwith "command failed"
+  fi
+  if [ "$actual" != "$expected" ]; then
+    echo "EXPECTED: $expected"
+    echo "ACTUAL: $actual"
+    failwith "assertion failed"
+  fi
+}
