1a0390f9fc0d941b1da1b139d2f2a77888cc4644
Andrey Popp <8mayday@gmail.com>
2017-11-23 16:51:18 +0300

    Add `esy export-dependencies` command
    This command exports dependencies of the current sandbox into `_export`
    directory.

---
+++ src/bin/esyExportDependencies.js
@@ -0,0 +1,58 @@
+/**
+ * @flow
+ */
+
+import type {CommandContext} from './esy';
+
+import {getSandbox, getBuildConfig} from './esy';
+import * as Task from '../build-task';
+import * as Sandbox from '../sandbox';
+import * as Env from '../environment';
+import * as Graph from '../graph.js';
+import * as Child from '../lib/child_process.js';
+import * as fs from '../lib/fs.js';
+
+const esyBin = require.resolve('../../bin/_esy');
+
+export default async function esyExport(ctx: CommandContext) {
+  const sandbox = await getSandbox(ctx);
+  const config = await getBuildConfig(ctx);
+
+  const toExport = [];
+  Graph.traverse(sandbox.root, build => {
+    switch (build.sourceType) {
+      case 'immutable':
+        toExport.push(build);
+        break;
+      case 'transient':
+        ctx.reporter.warn(
+          `${build.packagePath} is a linked dependency, skipping it for export...`,
+        );
+        break;
+      case 'root':
+        // do nothing
+        break;
+    }
+  });
+
+  const toExportMissing = [];
+  await Promise.all(
+    toExport.map(async build => {
+      const finalInstallPath = config.getFinalInstallPath(build);
+      if (!await fs.exists(finalInstallPath)) {
+        toExportMissing.push(build);
+      }
+    }),
+  );
+
+  if (toExportMissing.length > 0) {
+    ctx.error('unable to export some of the artefacts, run "esy build" command');
+  }
+
+  await Promise.all(
+    toExport.map(async build => {
+      const finalInstallPath = config.getFinalInstallPath(build);
+      await Child.spawn(esyBin, ['export-build', finalInstallPath], {stdio: 'inherit'});
+    }),
+  );
+}
