f734db59df0c4451390c7cc644c357f1487ca7f0
Andrey Popp <8mayday@gmail.com>
2017-11-29 13:41:03 +0300

    esyImportOpam: suggest possible opam files in case we can't find the right one

--- src/bin/esyImportOpam.js
+++ src/bin/esyImportOpam.js
@@ -6,8 +6,10 @@ import type {CommandContext, CommandInvocation} from './esy';
 
 import * as EsyOpam from '@esy-ocaml/esy-opam';
 import * as semver from 'semver';
+import outdent from 'outdent';
 
-import * as fs from '../lib/fs';
+import * as fs from '../lib/fs.js';
+import * as path from '../lib/path.js';
 
 const AVAILABLE_OCAML_COMPILERS = [['4.4.2000', '~4.4.2000'], ['4.2.3000', '~4.2.3000']];
 
@@ -17,7 +19,19 @@ export default async function importOpamCommand(
 ) {
   const [packageName, packageVersion, opamFilename] = invocation.args;
   if (opamFilename == null) {
-    ctx.error(`usage: esy import-opam PACKAGENAME PACKAGEVERSION OPAMFILENAME`);
+    ctx.error(`usage: esy import-opam <packagename> <packageversion> <opamfilename>`);
+  }
+  if (!await fs.exists(opamFilename)) {
+    const suggestions = await findOpamFiles(path.dirname(opamFilename));
+    const error = `File "${opamFilename}" doesn't exist`;
+    if (suggestions.length > 0) {
+      ctx.error(outdent`
+        ${error}, there are other opam files in the directory:
+        ${suggestions.map(file => `  "${file}"`).join('\n')}
+      `);
+    } else {
+      ctx.error(error);
+    }
   }
   const opamData = await fs.readFile(opamFilename);
   const opam = EsyOpam.parseOpam(opamData);
@@ -39,6 +53,17 @@ export default async function importOpamCommand(
   // $FlowFixMe: suppress therefore...
   delete packageJson._esy_opam_patches;
   console.log(JSON.stringify(packageJson, null, 2));
+}
+
+async function findOpamFiles(dirname) {
+  if (!await fs.exists(dirname)) {
+    return [];
+  }
+  const files = await fs.readdir(dirname);
+  const opamFiles = files
+    .filter(file => file === 'opam' || file.endsWith('.opam'))
+    .map(file => path.join(dirname, file));
+  return opamFiles;
 }
 
 export const noHeader = true;
