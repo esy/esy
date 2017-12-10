c8907d226c797f134e8cfb8f0661a1936d1d438a
Rauan Mayemir <rauan@maemirov.com>
2017-12-10 11:54:55 +0800

    improved `esy list-libs` tree formatting

--- src/bin/esyListLibs.js
+++ src/bin/esyListLibs.js
@@ -6,39 +6,45 @@ import type {CommandContext, CommandInvocation} from './esy';
 import type {BuildSpec, Config} from '../types';
 
 import chalk from 'chalk';
-import { find } from '../graph'
-import { spawn } from '../lib/child_process';
+import * as fs from '../lib/fs';
+import * as path from '../lib/path';
+import {find} from '../graph';
+import {spawn} from '../lib/child_process';
 
-import { getSandbox, getBuildConfig } from './esy';
-import { Promise } from '../lib/Promise';
+import {getSandbox, getBuildConfig} from './esy';
+import {Promise} from '../lib/Promise';
 
-export default async function esyListLibs(
+export default async function esyLsLibs(
   ctx: CommandContext,
   invocation: CommandInvocation,
 ) {
   const sandbox = await getSandbox(ctx);
   const config = await getBuildConfig(ctx);
 
-  const { flags } = invocation.options
+  const {flags} = invocation.options;
 
-  const ocamlfind = find(sandbox.root, (cur) => {
-    return cur.name === '@opam/ocamlfind'
+  const ocamlfind = find(sandbox.root, cur => {
+    return cur.name === '@opam/ocamlfind';
   });
 
   if (ocamlfind != null) {
-    const ocamlfindCmd = config.getFinalInstallPath(ocamlfind, 'bin', 'ocamlfind')
-    const builtIns = await getLibraryPackages(config, ocamlfindCmd)
+    const ocamlfindCmd = config.getFinalInstallPath(ocamlfind, 'bin', 'ocamlfind');
+    const builtIns = await getPackageLibraries(config, ocamlfindCmd);
 
-    console.log(await formatBuildSpecTree(config, sandbox.root, {
-      indent: 0,
-      seen: new Set(),
-      isLast: true,
-      showAll: !!flags.all,
-      ocamlfind: ocamlfindCmd,
-      builtIns
-    }));
+    console.log(
+      await formatBuildSpecTree(config, sandbox.root, {
+        indent: 0,
+        seen: new Set(),
+        isLast: true,
+        showAll: !!flags.all,
+        ocamlfind: ocamlfindCmd,
+        builtIns,
+      }),
+    );
   } else {
-    throw new Error('We couldn\'t find ocamlfind, consider adding it to your devDependencies');
+    throw new Error(
+      "We couldn't find ocamlfind, consider adding it to your devDependencies",
+    );
   }
 }
 
@@ -52,13 +58,13 @@ type FormatBuildSpecTreeCtx = {
   isLast: boolean,
   showAll: boolean,
   ocamlfind: string,
-  builtIns: Array<string>
+  builtIns: Array<string>,
 };
 
 async function formatBuildSpecTree(
   config: Config<*>,
   spec: BuildSpec,
-  ctx: FormatBuildSpecTreeCtx
+  ctx: FormatBuildSpecTreeCtx,
 ) {
   const {indent, seen, isLast, showAll} = ctx;
   const dependenciesLines = [];
@@ -73,58 +79,84 @@ async function formatBuildSpecTree(
 
       if (showAll || indent < 1) {
         dependenciesLines.push(
-          formatBuildSpecTree(config, dep, {...ctx, seen, indent: indent + 1}),
+          formatBuildSpecTree(config, dep, {...ctx, seen, isLast, indent: indent + 1}),
         );
       }
     }
   } else {
-    return ''
+    return '';
   }
 
   const version = chalk.grey(`@${spec.version}`);
-  const name = `${spec.name}${version}`;
+  let name = `${spec.name}${version}`;
+  if (indent > 1) {
+    name = chalk.grey(name);
+  }
+
   const prefix = indent === 0 ? '' : isLast ? '└── ' : '├── ';
-  let line = `${prefix}${name}`;
-  line = line.padStart(line.length + (indent - 1) * 4, '│   ');
+  const info = await formatBuildInfo(config, spec);
+  let pkg = `${prefix}${name} ${info}`;
+  pkg = pkg.padStart(pkg.length + (indent - 1) * 4, '│   ');
 
-  const packages = await formatBuildPackagesList(config, spec, ctx)
+  const libs = (await fs.exists(config.getFinalInstallPath(spec)))
+    ? await formatBuildLibrariesList(config, spec, {...ctx, indent: indent + 1})
+    : '';
 
-  return [line, packages]
+  return [pkg, libs]
     .concat(await Promise.all(dependenciesLines))
     .filter(line => line.length > 0)
     .join('\n');
 }
 
-async function formatBuildPackagesList(
+async function formatBuildLibrariesList(
   config: Config<*>,
   spec: BuildSpec,
-  ctx: FormatBuildSpecTreeCtx
+  ctx: FormatBuildSpecTreeCtx,
 ) {
-  const { indent, ocamlfind, builtIns } = ctx;
+  const {indent, ocamlfind, builtIns} = ctx;
+  const libraryLines = [];
 
-  const packages = await getLibraryPackages(config, ocamlfind, spec)
+  const libs = (await getPackageLibraries(config, ocamlfind, spec)).filter(
+    lib => builtIns.indexOf(lib) < 0,
+  );
 
-  return packages
-    .filter(pkg => builtIns.indexOf(pkg) < 0)
-    .map(pkg => {
-      let line = `    ${pkg}`;
-      line = line.padStart(line.length + (indent - 1) * 4, '    ');
-      return line;
-    }).join('\n');
+  for (const lib of libs) {
+    const isLast = lib === libs[libs.length - 1];
+    const name = indent > 2 ? chalk.yellow(`${lib}`) : chalk.yellow.bold(`${lib}`);
+
+    const prefix = indent === 0 ? '' : isLast ? '└── ' : '├── ';
+    const line = `${prefix}${name}`;
+
+    libraryLines.push(line.padStart(line.length + (indent - 1) * 4, '│   '));
+  }
+
+  return libraryLines.join('\n');
 }
 
-async function getLibraryPackages(config: Config<*>,
+async function getPackageLibraries(
+  config: Config<*>,
   ocamlfind: string,
-  spec: ?BuildSpec
+  spec: ?BuildSpec,
 ): Promise<Array<string>> {
   const result = await spawn(ocamlfind, ['list'], {
     env: {
-      'OCAMLPATH': spec != null ? config.getFinalInstallPath(spec, 'lib') : ''
-    }
-  })
+      OCAMLPATH: spec != null ? config.getFinalInstallPath(spec, 'lib') : '',
+    },
+  });
 
-  return result.split("\n").map((line: string) => {
-    const [pkg, ...version] = line.split(" ")
-    return pkg
-  })
+  return result.split('\n').map((line: string) => {
+    const [lib, ...version] = line.split(' ');
+    return lib;
+  });
+}
+
+async function formatBuildInfo(config, spec) {
+  const buildStatus = (await fs.exists(config.getFinalInstallPath(spec)))
+    ? chalk.green('[built]')
+    : chalk.blue('[build pending]');
+  let info = [buildStatus];
+  if (spec.sourceType === 'transient' || spec.sourceType === 'root') {
+    info.push(chalk.blue('[local source]'));
+  }
+  return info.join(' ');
 }
