af802c22b85ff35dd84f4953af65e96cf084621e
Rauan Mayemir <rauan@maemirov.com>
2017-12-11 18:40:19 +0800

    refactored common funcs for builds and libs tree, changed graph traversing to breadth-first

--- src/bin/esyLsBuilds.js
+++ src/bin/esyLsBuilds.js
@@ -4,61 +4,34 @@
 
 import type {CommandContext} from './esy';
 import type {BuildSpec, Config} from '../types';
+import type {FormatBuildSpecQueueItem} from '../cli-utils';
 
-import chalk from 'chalk';
-import * as fs from '../lib/fs';
-import * as path from '../lib/path';
-import {formatBuildInfo} from '../cli-utils';
+import {formatPackageInfo, getBuildSpecPackages} from '../cli-utils';
 
 import {getSandbox, getBuildConfig} from './esy';
+import {Promise} from '../lib/Promise';
 
 export default async function esyLsBuilds(ctx: CommandContext) {
   const sandbox = await getSandbox(ctx);
   const config = await getBuildConfig(ctx);
-  console.log(await formatBuildSpecTree(config, sandbox.root));
-}
 
-type FormatBuildSpecTreeCtx = {
-  indent: number,
-  seen: Set<string>,
-  isLast: boolean,
-};
+  const queue = getBuildSpecPackages(config, sandbox.root);
+
+  console.log(await formatBuildSpecTree(config, queue));
+}
 
 async function formatBuildSpecTree(
   config: Config<*>,
-  spec: BuildSpec,
-  ctx?: FormatBuildSpecTreeCtx = {indent: 0, seen: new Set(), isLast: false},
+  queue: Array<FormatBuildSpecQueueItem>,
 ) {
-  const {indent, seen, isLast} = ctx;
-  const dependenciesLines = [];
+  const lines = [];
 
-  const hasSeenIt = seen.has(spec.id);
-  if (!hasSeenIt) {
-    seen.add(spec.id);
-    const dependencies = Array.from(spec.dependencies.values());
-    for (const dep of dependencies) {
-      const isLast = dep === dependencies[dependencies.length - 1];
-      dependenciesLines.push(
-        formatBuildSpecTree(config, dep, {indent: indent + 1, seen, isLast}),
-      );
-    }
+  for (const cur of queue) {
+    const {spec, ctx} = cur;
+    const pkg = formatPackageInfo(config, spec, ctx);
+    lines.push(pkg);
   }
 
-  const version = chalk.grey(`@${spec.version}`);
-  let name = `${spec.name}${version}`;
-  if (indent > 0 && spec.sourceType === 'transient') {
-    const loc = path.relative(config.sandboxPath, config.getSourcePath(spec));
-    name = `${name} ${chalk.grey(loc)}`;
-  }
-  const info = await formatBuildInfo(config, spec);
-  name = `${name} ${info}`;
-  if (hasSeenIt) {
-    name = chalk.grey(name);
-  }
-
-  const prefix = indent === 0 ? '' : isLast ? '└── ' : '├── ';
-  let line = `${prefix}${name}`;
-  line = line.padStart(line.length + (indent - 1) * 4, '│   ');
-
-  return [line].concat(await Promise.all(dependenciesLines)).join('\n');
+  const tree = await Promise.all(lines);
+  return tree.join('\n');
 }
