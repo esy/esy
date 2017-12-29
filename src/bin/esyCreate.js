e5699adc71eab56b9f63327785899336ae2279c1
Rauan Mayemir <rauan@maemirov.com>
2017-12-29 13:40:34 +0800

    fix esy init when no template args passed

--- src/bin/esyCreate.js
+++ src/bin/esyCreate.js
@@ -2,17 +2,30 @@
  * @flow
  */
 
+import type {CommandContext, CommandInvocation} from './esy';
+
+import chalk from 'chalk';
+import * as child from '@esy-ocaml/esy-install/src/util/child';
 import * as path from 'path';
 import * as fs from 'fs';
-import type {CommandContext, CommandInvocation} from './esy';
+import commander from 'commander';
+import parse from 'cli-argparse';
 import runYarnCommand from './runYarnCommand';
-import * as child from '@esy-ocaml/esy-install/src/util/child';
 
 export default async function esyCreate(
   ctx: CommandContext,
   invocation: CommandInvocation,
 ) {
-  const [builderName = 'esy-project', ...rest] = invocation.args;
+  const {unparsed} = parse(invocation.args);
+  let builderName;
+  let builderArgs;
+
+  if (unparsed.length) {
+    [builderName, ...builderArgs] = invocation.args;
+  } else {
+    builderName = 'esy-project';
+    builderArgs = invocation.args;
+  }
 
   const packageName = builderName.replace(/^(@[^\/]+\/)?/, '$1create-');
   const commandName = packageName.replace(/^@[^\/]+\//, '');
@@ -30,7 +43,7 @@ export default async function esyCreate(
   const binFolder = path.resolve(globalFolder, 'node_modules', '.bin');
   const command = path.resolve(binFolder, path.basename(commandName));
 
-  await child.spawn(command, [...rest], {stdio: `inherit`, shell: true});
+  await child.spawn(command, [...builderArgs], {stdio: `inherit`, shell: true});
 }
 
 export const noParse = true;
