09858789d43593188d32e40101146ab5588b327c
Andrey Popp <8mayday@gmail.com>
2018-01-04 17:52:38 +0300

    Fix bootstrap to build esy-build-package first

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -8,4 +8,14 @@ set -o pipefail
 # Spit out config for Esy executables implemented in bash
 #
 
+set +e
 (cd bin && node ./esy.js autoconf > ./esyConfig.sh)
+ret=$?
+set -e
+
+if [ $ret -ne 0 ]; then
+  echo "error:"
+  cat bin/esyConfig.sh
+  exit 1
+fi
+
