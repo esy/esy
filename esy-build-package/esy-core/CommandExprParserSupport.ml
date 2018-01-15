dafb8e2b5cea9da5f052a9566bfecbb889b45072
Andrey Popp <8mayday@gmail.com>
2018-01-15 15:03:35 +0300

    CommandExprParser: support namespaces

--- esy-build-package/esy-core/CommandExprParserSupport.ml
+++ esy-build-package/esy-core/CommandExprParserSupport.ml
@@ -12,14 +12,25 @@ and token =
   [@@deriving (show, eq)]
 
 and expr =
-  | Var of string
+  | Var of name
   | EnvVar of string
   | Literal of string
   | Colon
   | PathSep
   [@@deriving (show, eq)]
 
+and name = string list
+
 let buf_from_str str =
   let buf = Buffer.create 16 in
   Buffer.add_string buf str;
   buf
+
+let nested rule lexbuf =
+  let value  = Lexing.lexeme lexbuf in
+  let lexbuf = Lexing.from_string value in
+  let rec read tokens =
+    match rule lexbuf with
+    | `Token tok -> read (tok::tokens)
+    | `EOF -> List.rev tokens
+  in read []
--- esy-build-package/esy-test/TestCommandExpr.ml
+++ esy-build-package/esy-test/TestCommandExpr.ml
@@ -34,30 +34,33 @@ let%test "string with dquote" =
   expectParseOk "somet \"ok\"hing" [String "somet \"ok\"hing"]
 
 let%test "parse simple var" =
-  expectParseOk "#{hi}" [Expr [Var "hi"]] &&
-  expectParseOk "#{hi }" [Expr [Var "hi"]] &&
-  expectParseOk "#{ hi}" [Expr [Var "hi"]]
+  expectParseOk "#{hi}" [Expr [Var ["hi"]]] &&
+  expectParseOk "#{hi }" [Expr [Var ["hi"]]] &&
+  expectParseOk "#{ hi}" [Expr [Var ["hi"]]]
 
 let%test "parse var+" =
-  expectParseOk "#{hi}#{world}" [Expr [Var "hi"]; Expr [Var "world"]]
+  expectParseOk "#{hi}#{world}" [
+    Expr [Var ["hi"]];
+    Expr [Var ["world"]]
+    ]
 
 let%test "parse string + var" =
-  expectParseOk "hello #{world}" [String "hello "; Expr [Var "world"]] &&
-  expectParseOk " #{world}" [String " "; Expr [Var "world"]] &&
-  expectParseOk "#{world} " [Expr [Var "world"]; String " "] &&
-  expectParseOk "hello#{world}" [String "hello"; Expr [Var "world"]] &&
-  expectParseOk "#{hello} world" [Expr [Var "hello"]; String " world"] &&
-  expectParseOk "#{hello}world" [Expr [Var "hello"]; String "world"]
+  expectParseOk "hello #{world}" [String "hello "; Expr [Var ["world"]]] &&
+  expectParseOk " #{world}" [String " "; Expr [Var ["world"]]] &&
+  expectParseOk "#{world} " [Expr [Var ["world"]]; String " "] &&
+  expectParseOk "hello#{world}" [String "hello"; Expr [Var ["world"]]] &&
+  expectParseOk "#{hello} world" [Expr [Var ["hello"]]; String " world"] &&
+  expectParseOk "#{hello}world" [Expr [Var ["hello"]]; String "world"]
 
 let%test "parse complex var" =
-  expectParseOk "#{hi world}" [Expr [Var "hi"; Var "world"]] &&
-  expectParseOk "#{hi :}" [Expr [Var "hi"; Colon]] &&
-  expectParseOk "#{hi : world}" [Expr [Var "hi"; Colon; Var "world"]] &&
-  expectParseOk "#{hi /}" [Expr [Var "hi"; PathSep]] &&
-  expectParseOk "#{hi / world}" [Expr [Var "hi"; PathSep; Var "world"]]
+  expectParseOk "#{hi world}" [Expr [Var ["hi"]; Var ["world"]]] &&
+  expectParseOk "#{hi :}" [Expr [Var ["hi"]; Colon]] &&
+  expectParseOk "#{hi : world}" [Expr [Var ["hi"]; Colon; Var ["world"]]] &&
+  expectParseOk "#{hi /}" [Expr [Var ["hi"]; PathSep]] &&
+  expectParseOk "#{hi / world}" [Expr [Var ["hi"]; PathSep; Var ["world"]]]
 
 let%test "parse var with env vars" =
-  expectParseOk "#{hi / $world}" [Expr [Var "hi"; PathSep; EnvVar "world"]]
+  expectParseOk "#{hi / $world}" [Expr [Var ["hi"]; PathSep; EnvVar "world"]]
 
 let%test "parse var with literals" =
   expectParseOk "#{'world'}" [Expr [Literal "world"]] &&
@@ -69,8 +72,18 @@ let%test "parse var with literals" =
   expectParseOk "#{'world' :}" [Expr [Literal "world"; Colon]] &&
   expectParseOk "#{'world'/}" [Expr [Literal "world"; PathSep]] &&
   expectParseOk "#{'world':}" [Expr [Literal "world"; Colon]] &&
-  expectParseOk "#{hi'world'}" [Expr [Var "hi"; Literal "world"]] &&
-  expectParseOk "#{'world'hi}" [Expr [Literal "world"; Var "hi"]] &&
-  expectParseOk "#{hi / 'world'}" [Expr [Var "hi"; PathSep; Literal "world"]] &&
+  expectParseOk "#{hi'world'}" [Expr [Var ["hi"]; Literal "world"]] &&
+  expectParseOk "#{'world'hi}" [Expr [Literal "world"; Var ["hi"]]] &&
+  expectParseOk "#{hi / 'world'}" [Expr [Var ["hi"]; PathSep; Literal "world"]] &&
   expectParseOk "#{'hi''world'}" [Expr [Literal "hi";  Literal "world"]] &&
   expectParseOk "#{'h\\'i'}" [Expr [Literal "h'i"]]
+
+let%test "parse namespace" =
+  expectParseOk "#{ns.hi}" [Expr [Var ["ns"; "hi"]]] &&
+  expectParseOk "#{ns.hi.hey}" [Expr [Var ["ns"; "hi"; "hey"]]] &&
+  expectParseOk "#{@scope/pkg.hi}" [Expr [Var ["@scope/pkg"; "hi"]]] &&
+  expectParseOk "#{@scope/pkg.hi.hey}" [Expr [Var ["@scope/pkg"; "hi"; "hey"]]] &&
+  expectParseOk "#{@scope/pkg.hi 'hey'}" [Expr [
+    Var ["@scope/pkg"; "hi"];
+    Literal ("hey");
+  ]]
