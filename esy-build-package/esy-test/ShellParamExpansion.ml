2929f1bf8808c3c1da42efe980c93a28c043be22
Andrey Popp <8mayday@gmail.com>
2018-01-16 13:41:48 +0300

    ShellParamExpansion: render

--- esy-build-package/esy-test/ShellParamExpansion.ml
+++ esy-build-package/esy-test/ShellParamExpansion.ml
@@ -36,3 +36,27 @@ let%test "${var:-default}" =
   expectParseOk "string ${var:-def}" [String "string "; Var ("var", Some "def")] &&
   expectParseOk "${var:-def}string" [Var ("var", Some "def"); String "string"] &&
   expectParseOk "string${var:-def}" [String "string"; Var ("var", Some "def")]
+
+let expectRenderOk scope s expectedResult =
+  match render ~scope s with
+  | Ok result ->
+    if result <> expectedResult then (
+      Printf.printf "Expected: %s\n" expectedResult;
+      Printf.printf "     Got: %s\n" result;
+      false
+    ) else
+      true
+  | Error err ->
+    let msg = Printf.sprintf "Error: %s\nWhile parsing: %s" err s in
+    print_endline msg;
+    false
+
+let%test "render" =
+  let scope = function
+    | "name" -> Some "world"
+    | _ -> None
+  in
+  expectRenderOk scope "hello" "hello" &&
+  expectRenderOk scope "hello, $name" "hello, world" &&
+  expectRenderOk scope "hello, ${name}!" "hello, world!" &&
+  expectRenderOk scope "hello, ${nam:-world}!" "hello, world!"
