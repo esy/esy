18ecc10c6f652a4e94567a4ea363a19ccf1d530c
Andrey Popp <8mayday@gmail.com>
2018-01-16 13:41:48 +0300

    ShellParamExpansion: render

--- esy-build-package/esy-core/ShellParamExpansion.ml
+++ esy-build-package/esy-core/ShellParamExpansion.ml
@@ -10,3 +10,20 @@ let parse v =
   | UnmatchedChar (pos, c) ->
     let msg = Printf.sprintf "unknown char: '%c' at position %d" c pos.Lexing.pos_cnum
     in Error msg
+
+type scope = string -> string option
+
+let render ~(scope : scope) v =
+  let module Let_syntax = EsyLib.Result.Let_syntax in
+  let%bind tokens = parse v in
+  let f segments = function
+    | String v -> Ok (v::segments)
+    | Var (name, default) ->
+      begin match scope name, default with
+      | Some v, _
+      | None, Some v -> Ok (v::segments)
+      | _, _ -> Error ("unable to resolve: $" ^ name)
+      end
+  in
+  let%bind segments = EsyLib.Result.listFoldLeft ~f ~init:[] tokens in
+  Ok (segments |> List.rev |> String.concat "")
