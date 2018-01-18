576b0c0db9f73802a9e4a5adb535064fc34d2a74
Andrey Popp <8mayday@gmail.com>
2018-01-18 13:19:20 +0300

    PackageBuilder: fix exec, docs

--- esy-build-package/esy-core/PackageBuilder.mli
+++ esy-build-package/esy-core/PackageBuilder.mli
@@ -1,6 +1,19 @@
+(**
+ * This module describes methods which are performed on build tasks through
+ * "esy-build-package" package builder executable.
+ *)
 
+(**
+ * Build task.
+ *)
 val build : BuildTask.t -> unit RunAsync.t
 
+(*
+ * Spawn an interactive shell inside tbuild environment of the task.
+ *)
 val buildShell : BuildTask.t -> unit RunAsync.t
 
+(*
+ * Execute a command inside build environment of the task.
+ *)
 val buildExec : BuildTask.t -> unit RunAsync.t
