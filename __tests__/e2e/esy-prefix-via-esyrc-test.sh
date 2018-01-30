19681e00012e4d1e29d89a836b8f89a05d665300
Andrey Popp <8mayday@gmail.com>
2018-01-30 15:52:42 +0300

    tests: fix for Linux
    Use /tmp

--- __tests__/e2e/esy-prefix-via-esyrc-test.sh
+++ __tests__/e2e/esy-prefix-via-esyrc-test.sh
@@ -4,10 +4,10 @@ doTest () {
   unset ESY__PREFIX
 
   rm -rf /tmp/custom-esy-prefix
-  echo 'esy-prefix-path: "/private/tmp/custom-esy-prefix"' > ./.esyrc
+  echo 'esy-prefix-path: "/tmp/custom-esy-prefix"' > ./.esyrc
   export OCAMLRUNPARAM=b
   run esy build
   expectStdout "dep" esy dep
-  run esy which dep | grep "/private/tmp/custom-esy-prefix" || \
+  run esy which dep | grep "/tmp/custom-esy-prefix" || \
     (echo "Not inside the configured prefix" && exit 1)
 }
