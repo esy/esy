7fa17e8537927591f729b1ffa971f62cd96cbb0d
Andrey Popp <8mayday@gmail.com>
2018-02-01 20:34:09 +0300

    Fix #{self.*} expansion inside "esy.exportedEnv"
    it was expanded to stage paths `%strore%/s/...` instead of install paths
    `%store%/i/...` which is incorrect — exported env is used by dependent
    packages when those are already built and installed.

---
+++ esy-core/test/BuildTask.ml
@@ -0,0 +1,82 @@
+open Esy
+
+let cfg = {
+  Config.
+  esyVersion = "0.x.x";
+  sandboxPath = Path.v "/tmp/__sandbox__";
+  prefixPath = Path.v "/tmp/__prefix__";
+  storePath = Path.v "/tmp/__store__";
+  localStorePath = Path.v "/tmp/__local_store__";
+}
+
+module TestCommandExpr = struct
+  let dep = Package.{
+    id = "%dep%";
+    name = "dep";
+    version = "1.0.0";
+    dependencies = [];
+    buildCommands = None;
+    installCommands = None;
+    buildType = BuildType.InSource;
+    sourceType = SourceType.Immutable;
+    exportedEnv = [
+      {
+        ExportedEnv.
+        name = "OK";
+        value = "#{self.install / 'ok'}";
+        exclusive = false;
+        scope = Local;
+      }
+    ];
+    sourcePath = Config.ConfigPath.ofPath cfg (Path.v "/path");
+  }
+
+  let pkg = Package.{
+    id = "%pkg%";
+    name = "pkg";
+    version = "1.0.0";
+    dependencies = [Dependency dep];
+    buildCommands = Some [CommandList.Command.Unparsed "cp ./hello #{self.bin}"];
+    installCommands = Some [CommandList.Command.Parsed ["cp"; "./man"; "#{self.man}"]];
+    buildType = BuildType.InSource;
+    sourceType = SourceType.Immutable;
+    exportedEnv = [];
+    sourcePath = Config.ConfigPath.ofPath cfg (Path.v "/path");
+  }
+
+  let task = BuildTask.ofPackage pkg
+
+  let check f =
+    match task with
+    | Ok task ->
+      f task
+    | Error err ->
+      print_endline (Run.formatError err);
+      false
+
+  let%test "#{self...} inside esy.build" =
+    check (fun task ->
+      let commands = BuildTask.CommandList.show task.buildCommands in
+      commands = {|[["cp"; "./hello"; "%store%/s/%pkg%/bin"]]|}
+    )
+
+  let%test "#{self...} inside esy.install" =
+    check (fun task ->
+      let commands = BuildTask.CommandList.show task.installCommands in
+      commands = {|[["cp"; "./man"; "%store%/s/%pkg%/man"]]|}
+    )
+
+  let%test "#{self...} inside esy.exportedEnv" =
+    check (fun task ->
+      let bindings = Environment.Closed.bindings task.env in
+      let f = function
+        | {Environment. name = "OK"; value = Value value; _} ->
+          value = "%store%/i/%dep%/ok"
+        | _ ->
+          false
+      in
+      match ListLabels.find_opt ~f bindings with
+      | Some _ -> true
+      | None -> false
+    )
+end
