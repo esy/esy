299de93842958b052f706b3fe47f4af1bbf17330
Andrey Popp <8mayday@gmail.com>
2018-02-20 14:54:56 +0300

    Parse buildTimeDependencies out of package.json

--- esy-core/esy/Package.ml
+++ esy-core/esy/Package.ml
@@ -163,6 +163,7 @@ module Manifest = struct
     peerDependencies : (ManifestDependencyMap.t [@default StringMap.empty]);
     devDependencies : (ManifestDependencyMap.t [@default StringMap.empty]);
     optDependencies : (ManifestDependencyMap.t [@default StringMap.empty]);
+    buildTimeDependencies : (ManifestDependencyMap.t [@default StringMap.empty]);
     esy: EsyManifest.t option [@default None];
     _resolved: (string option [@default None]);
   } [@@deriving (show, of_yojson { strict = false })]
@@ -213,6 +214,7 @@ and dependency =
   | PeerDependency of t
   | OptDependency of t
   | DevDependency of t
+  | BuildTimeDependency of t
   | InvalidDependency of {
     pkgName: string;
     reason: string;
@@ -240,6 +242,7 @@ module DependencyGraph = DependencyGraph.Make(struct
       | Dependency pkg
       | OptDependency pkg
       | DevDependency pkg
+      | BuildTimeDependency pkg
       | PeerDependency pkg -> (pkg, dep)::acc
       | InvalidDependency _ -> acc
     in
@@ -252,6 +255,7 @@ end)
 let traverseImmutableDependencies (pkg : t) =
   let f deps dep = match dep with
     | Dependency ({ sourceType = SourceType.Immutable; _ } as pkg)
+    | BuildTimeDependency ({ sourceType = SourceType.Immutable; _ } as pkg)
     | OptDependency ({ sourceType = SourceType.Immutable; _ } as pkg)
     | PeerDependency ({ sourceType = SourceType.Immutable; _ } as pkg) ->
       (pkg, dep)::deps
@@ -259,6 +263,7 @@ let traverseImmutableDependencies (pkg : t) =
     | OptDependency _
     | PeerDependency _
     | DevDependency _
+    | BuildTimeDependency _
     | InvalidDependency _ -> deps
   in
   pkg.dependencies
