6b466b43a213a7eb0602669daa4dfa521ade5d72
Andrey Popp <8mayday@gmail.com>
2018-02-22 22:01:07 +0300

    Add test for augmenting $PATH

---
+++ __tests__/build/build-augment-path-test.sh
@@ -0,0 +1,9 @@
+doTest () {
+  initFixture ./fixtures/augment-path
+  run esy build
+
+  # package "dep" should be visible in all envs
+  expectStdout "dep" esy dep
+  expectStdout "dep" esy b dep
+  expectStdout "dep" esy x dep
+}
