3d943be28b1360a24436f7c5fa33bffb786da07f
Andrey Popp <8mayday@gmail.com>
2018-03-07 23:28:15 +0300

    test-e2e: move opam test out
    This is not going to be run at CI for now

--- test-e2e/export-import-build/export-import-symlinks-into-dep-test.sh
+++ test-e2e/export-import-build/export-import-symlinks-into-dep-test.sh
@@ -19,7 +19,7 @@ doTest () {
   # check that link is here
   storeTarget=$(readlink ../esy/3/i/dep-1.0.0-*/bin/dep)
   if [[ "$storeTarget" != $ESY__PREFIX* ]]; then
-    failwith "invalid symlink target: $storeTarget"
+    failwith "(1) invalid symlink target: $storeTarget"
   fi
 
   # export build from store
@@ -30,8 +30,9 @@ doTest () {
 
   # check symlink target for exported build
   exportedTarget=$(readlink _export/dep-1.0.0-*/bin/dep)
+  echo $exportedTarget
   if [[ "$exportedTarget" != ________* ]]; then
-    failwith "invalid symlink target: $exportedTarget"
+    failwith "(2) invalid symlink target: $exportedTarget"
   fi
 
   # drop & import
@@ -41,6 +42,6 @@ doTest () {
   # check symlink target for imported build
   importedTarget=$(readlink ../esy/3/i/dep-1.0.0-*/bin/dep)
   if [[ "$importedTarget" != $ESY__PREFIX* ]]; then
-    failwith "invalid symlink target: $exportedTarget"
+    failwith "(3) invalid symlink target: $exportedTarget"
   fi
 }
