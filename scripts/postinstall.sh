fc90d370eb183f1d40e6a2369da3f83932679d7b
Andrey Popp <8mayday@gmail.com>
2018-05-24 15:38:58 +0300

    Fix postinstall.sh script

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -8,11 +8,13 @@ case $(uname) in
   Darwin*)
     (cd _build/default && \
       mv esy/bin/esyCommand-darwin.exe esy/bin/esyCommand.exe && \
+      mv esyi/bin/esyi-darwin.exe esyi/bin/esyi.exe && \
       mv esy-build-package/bin/esyBuildPackageCommand-darwin.exe esy-build-package/bin/esyBuildPackageCommand.exe)
     ;;
   Linux*)
     (cd _build/default && \
       mv esy/bin/esyCommand-linux.exe esy/bin/esyCommand.exe && \
+      mv esyi/bin/esyi-linux.exe esyi/bin/esyi.exe && \
       mv esy-build-package/bin/esyBuildPackageCommand-linux.exe esy-build-package/bin/esyBuildPackageCommand.exe)
     ;;
   *)
