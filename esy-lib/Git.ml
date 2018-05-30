6a3448bbe6828626d4000862a541ff2caee48512
Andrey Popp <8mayday@gmail.com>
2018-05-30 16:56:04 +0300

    esy-lib: improvements to Git

--- esy-lib/Git.ml
+++ esy-lib/Git.ml
@@ -1,8 +1,24 @@
 
-let clone ~dst ~remote =
+type ref = string
+
+type remote = string
+
+let clone ~dst ~remote () =
   let cmd = Cmd.(v "git" % "clone" % remote % p dst) in
   ChildProcess.run cmd
 
-let checkout ~ref ~repo =
+let checkout ~ref ~repo () =
   let cmd = Cmd.(v "git" % "-C" % p repo % "checkout" % ref) in
   ChildProcess.run cmd
+
+let lsRemote ?ref ~remote () =
+  let open RunAsync.Syntax in
+  let ref = Option.orDefault "master" ref in
+  let cmd = Cmd.(v "git"  % "ls-remote" % remote % ref) in
+  let%bind out = ChildProcess.runOut cmd in
+  match out |> String.trim |> String.split_on_char '\n' with
+  | [] ->
+    let msg = Printf.sprintf "Unable to resolve ref: %s" ref in
+    error msg
+  | line::_ ->
+    return (line |> String.split_on_char '\t' |> List.hd)
--- esy-lib/Git.mli
+++ esy-lib/Git.mli
@@ -4,14 +4,28 @@
  * The implementation uses git command.
  *)
 
+type ref = string
+
+type remote = string
+
 (** Clone repositoryu from [remote] into [dst] local path. *)
 val clone :
   dst:Fpath.t
-  -> remote:string
+  -> remote:remote
+  -> unit
   -> unit RunAsync.t
 
 (** Checkout the [ref] in the [repo] *)
 val checkout :
-  ref:string
+  ref:ref
   -> repo:Fpath.t
+  -> unit
   -> unit RunAsync.t
+
+
+  (** Resolve [ref] of the [remote] *)
+val lsRemote :
+  ?ref:ref
+  -> remote:remote
+  -> unit
+  -> string RunAsync.t
