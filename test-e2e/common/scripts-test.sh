0d539d78b1bff514c0d34b022a1e3fd59ef020dd
Rauan Mayemir <rauan@maemirov.com>
2018-06-01 03:59:55 +0600

    Refactor makeExecCommand to lookup scripts only in sandbox env

--- test-e2e/common/scripts-test.sh
+++ test-e2e/common/scripts-test.sh
@@ -8,12 +8,11 @@ doTest() {
   expectStdout "cmd1_result" esy cmd1
   expectStdout "cmd2_result" esy cmd2
   expectStdout "cmd_array_result" esy cmd3
+  runAndExpectFailure esy b cmd1
+  runAndExpectFailure esy x cmd1
 
-  expectStdout "script_exec_result" esy x cmd4
-  expectStdout "script_exec_result" esy exec_cmd4
-  expectStdout "script_exec_result" esy exec_cmd4_cmd
+  expectStdout "script_exec_result" esy exec_cmd1
+  expectStdout "script_exec_result" esy exec_cmd2
 
-  expectStdout "build_cmd1_result" esy b build_cmd1
-  expectStdout "build_cmd2_result" esy b build_cmd2
-  expectStdout "build_cmd3_result" esy build_cmd3
+  expectStdout "build_cmd_result" esy build_cmd
 }
