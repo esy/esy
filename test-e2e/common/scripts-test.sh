1eb689451afc5099063e029d9c32e44c62863ba2
Rauan Mayemir <rauan@maemirov.com>
2018-05-31 23:11:58 +0600

    Add scripts tests for exec env

--- test-e2e/common/scripts-test.sh
+++ test-e2e/common/scripts-test.sh
@@ -3,7 +3,13 @@
 doTest() {
   initFixture ./fixtures/scripts-workflow
 
+  run esy build
+
   expectStdout "cmd1_result" esy cmd1
   expectStdout "cmd2_result" esy cmd2
   expectStdout "cmd_array_result" esy cmd3
+
+  expectStdout "script_exec_result" esy x cmd4
+  expectStdout "script_exec_result" esy exec_cmd4
+  expectStdout "script_exec_result" esy exec_cmd4_cmd
 }
