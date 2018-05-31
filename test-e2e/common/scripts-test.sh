664aaaae4d2efa525f6c741428f6143ed653592e
Rauan Mayemir <rauan@maemirov.com>
2018-05-31 23:49:28 +0600

    Add tests for scripts running in build env

--- test-e2e/common/scripts-test.sh
+++ test-e2e/common/scripts-test.sh
@@ -12,4 +12,8 @@ doTest() {
   expectStdout "script_exec_result" esy x cmd4
   expectStdout "script_exec_result" esy exec_cmd4
   expectStdout "script_exec_result" esy exec_cmd4_cmd
+
+  expectStdout "build_cmd1_result" esy b build_cmd1
+  expectStdout "build_cmd2_result" esy b build_cmd2
+  expectStdout "build_cmd3_result" esy build_cmd3
 }
