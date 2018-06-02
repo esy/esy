49fc341a2b5f9c499b9a91ff265a255bb1077136
Andrey Popp <8mayday@gmail.com>
2018-06-02 16:02:19 +0300

    esy-lib: add Cli.cmdTerm and Cli.cmdOptionTerm
    Those two terms allow to abstract away command line parsing logic
    related to passing/executing commands.

--- esy-lib/Cli.ml
+++ esy-lib/Cli.ml
@@ -3,3 +3,33 @@ let pathConv =
   let parse = Path.of_string in
   let print = Path.pp in
   Arg.conv ~docv:"PATH" (parse, print)
+
+let cmdTerm ~doc ~docv =
+  let open Cmdliner in
+  let commandTerm =
+    Arg.(non_empty & (pos_all string []) & (info [] ~doc ~docv))
+  in
+  let d command =
+    match command with
+    | [] ->
+      `Error (false, "command cannot be empty")
+    | tool::args ->
+      let cmd = Cmd.(v tool |> addArgs args) in
+      `Ok cmd
+  in
+  Term.(ret (const d $ commandTerm))
+
+let cmdOptionTerm ~doc ~docv =
+  let open Cmdliner in
+  let commandTerm =
+    Arg.(value & (pos_all string []) & (info [] ~doc ~docv))
+  in
+  let d command =
+    match command with
+    | [] ->
+      `Ok None
+    | tool::args ->
+      let cmd = Cmd.(v tool |> addArgs args) in
+      `Ok (Some cmd)
+  in
+  Term.(ret (const d $ commandTerm))
