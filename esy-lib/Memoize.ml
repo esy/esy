d02b118704617e0cf2382eb001c12c4af470714c
Andrey Popp <8mayday@gmail.com>
2018-06-06 17:33:55 +0300

    esy-lib: functorized API for Memoize

--- esy-lib/Memoize.ml
+++ esy-lib/Memoize.ml
@@ -1,8 +1,69 @@
-type ('k, 'v) t = ('k, 'v) Hashtbl.t
+module type MEMOIZEABLE = sig
+  type key
+  type value
+end
+
+module type MEMOIZE = functor (C: MEMOIZEABLE) -> sig
+  type t
+
+  type key = C.key
+  type value = C.value
+
+  val make : ?size:int -> unit -> t
+  val compute : t -> key -> (key -> value) -> value
+  val ensureComputed : t -> key -> (key -> value) -> unit
+  val put : t -> key -> value -> unit
+  val get : t -> key -> value option
+  val values : t -> (key * value) list
+end
+
+module Make : MEMOIZE = functor (C : MEMOIZEABLE) -> struct
+  type key = C.key
+  type value = C.value
+  type t = (key, value) Hashtbl.t
+
+  let make ?(size=200) () =
+    let cache = Hashtbl.create size in
+    cache
+
+  let put cache k v =
+    Hashtbl.replace cache k v
+
+  let get = Hashtbl.find_opt
+
+  let values cache =
+    let f k v values =
+      (k, v)::values
+    in Hashtbl.fold f cache []
+
+  let compute cache k compute =
+    try Hashtbl.find cache k with
+    | Not_found ->
+      let v = compute k in
+      Hashtbl.add cache k v;
+      v
+
+  let ensureComputed cache k compute =
+    if not (Hashtbl.mem cache k)
+    then Hashtbl.add cache k (compute k)
+
+end
+
+type ('key, 'value) t = ('key, 'value) Hashtbl.t
 
 let make ?(size=200) () =
   let cache = Hashtbl.create size in
   cache
+
+let put cache k v =
+  Hashtbl.replace cache k v
+
+let get = Hashtbl.find_opt
+
+let values cache =
+  let f k v values =
+    (k, v)::values
+  in Hashtbl.fold f cache []
 
 let compute cache k compute =
   try Hashtbl.find cache k with
@@ -10,3 +71,7 @@ let compute cache k compute =
     let v = compute k in
     Hashtbl.add cache k v;
     v
+
+let ensureComputed cache k compute =
+  if not (Hashtbl.mem cache k)
+  then Hashtbl.add cache k (compute k)
--- esy-lib/Memoize.mli
+++ esy-lib/Memoize.mli
@@ -1,12 +1,33 @@
+(** Parametrized API *)
+
 type ('k, 'v) t
+val make : ?size:int -> unit -> ('k, 'v) t
+val compute : ('k, 'v) t -> 'k -> ('k -> 'v) -> 'v
+val ensureComputed : ('k, 'v) t -> 'k -> ('k -> 'v) -> unit
+val put : ('k, 'v) t -> 'k -> 'v -> unit
+val get : ('k, 'v) t -> 'k -> 'v option
+val values : ('k, 'v) t -> ('k * 'v) list
 
-val make :
-  ?size:int
-  -> unit
-  -> ('k, 'v) t
+(** Functorized API *)
 
-val compute :
-  ('k, 'v) t
-  -> 'k
-  -> ('k -> 'v)
-  -> 'v
+module type MEMOIZEABLE = sig
+  type key
+  type value
+end
+
+module type MEMOIZE = functor (C: MEMOIZEABLE) -> sig
+  type t
+
+  type key = C.key
+  type value = C.value
+
+  val make : ?size:int -> unit -> t
+  val compute : t -> key -> (key -> value) -> value
+  val ensureComputed : t -> key -> (key -> value) -> unit
+  val put : t -> key -> value -> unit
+  val get : t -> key -> value option
+  val values : t -> (key * value) list
+end
+
+module Make : MEMOIZE
+
