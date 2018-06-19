6484f4436a0f163956492bfec807bcd8801dafd7
Andrey Popp <8mayday@gmail.com>
2018-06-19 14:01:45 +0300

    esyi: doc

--- esyi/Resolver.mli
+++ esyi/Resolver.mli
@@ -14,6 +14,8 @@ type t
 (** Make new resolver *)
 val make : cfg:Config.t -> unit -> t RunAsync.t
 
-(** Resolve package request into a list of packages *)
+(** Resolve package request into a list of resolutions *)
 val resolve : req:PackageInfo.Req.t -> t -> Resolution.t list RunAsync.t
+
+(** Fetch the package given the resolution. *)
 val package : resolution:Resolution.t -> t -> Package.t RunAsync.t
