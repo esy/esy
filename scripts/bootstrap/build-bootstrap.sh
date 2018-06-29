376a083c8e07f3b25f69e29b09cca222563dd974
Bryan Phelps <bryphe@outlook.com>
2018-06-28 18:12:07 -0700

    Retry make bootstrap

--- scripts/bootstrap/build-bootstrap.sh
+++ scripts/bootstrap/build-bootstrap.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env sh
 
 cp scripts/bootstrap/Makefile.bootstrap Makefile
+
+# Not sure why this needs to be run multiple times..
+# https://github.com/esy/esy/issues/213
 make bootstrap
+make bootstrap
+make bootstrap
+
 make build-release
