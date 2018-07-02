77f74c5bd48bf085452aaa486083e1026556d6ef
Andrey Popp <8mayday@gmail.com>
2018-07-02 21:48:31 +0300

    esyi: unpack zip archives with unzip

--- esy-lib/Tarball.ml
+++ esy-lib/Tarball.ml
@@ -1,4 +1,4 @@
-let unpack ?stripComponents ~dst filename =
+let unpackWithTar ?stripComponents ~dst filename =
   let cmd =
     let cmd = Cmd.(v "tar" % "xf" % p filename) in
     let cmd =
@@ -11,6 +11,39 @@ let unpack ?stripComponents ~dst filename =
     cmd
   in
   ChildProcess.run cmd
+
+let unpackWithUnzip ?stripComponents ~dst filename =
+  let open RunAsync.Syntax in
+  Fs.withTempDir begin fun out ->
+    let cmd = Cmd.(v "unzip" % "-d" % p out % p filename) in
+    let%bind () = ChildProcess.run cmd in
+    let%bind out =
+      match stripComponents with
+      | None -> return out
+      | Some n ->
+        let rec find path = function
+          | 0 -> return path
+          | n ->
+            begin match%bind Fs.listDir path with
+            | [item] -> find Path.(path / item) (n - 1)
+            | [] -> error "unpackWithUnzip: unable to strip path components"
+            | _ -> error "unpackWithUnzip: unable to strip path components"
+            end
+        in
+        find out n
+    in
+    let%bind items = Fs.listDir out in
+    let%bind () =
+      let f item = Fs.copyFile ~src:Path.(out / item) ~dst:Path.(dst / item) in
+      items |> List.map ~f |> RunAsync.List.waitAll
+    in
+    return ()
+  end
+
+let unpack ?stripComponents ~dst filename =
+  match Path.get_ext filename with
+  | ".zip" -> unpackWithUnzip ?stripComponents ~dst filename
+  | _ -> unpackWithTar ?stripComponents ~dst filename
 
 let create ~filename src =
   let cmd = Cmd.(v "tar" % "czf" % p filename % "-C" % p src % ".") in
