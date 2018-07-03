a81dd8114308c39079d663b173aef04a9b94f5da
Andrey Popp <8mayday@gmail.com>
2018-07-03 17:48:38 +0300

    esyi: do not print on stdout during patching

--- esy-lib/Patch.ml
+++ esy-lib/Patch.ml
@@ -1,3 +1,27 @@
+let runPatch cmd =
+  let f p =
+    let%lwt stdout = Lwt_io.read p#stdout
+    and stderr = Lwt_io.read p#stderr in
+    match%lwt p#status with
+    | Unix.WEXITED 0 ->
+      RunAsync.return ()
+    | _ ->
+      Logs_lwt.err (fun m -> m
+        "@[<v>command failed: %a@\nstderr:@[<v 2>@\n%s@]@\nstdout:@[<v 2>@\n%s@]@]"
+        Cmd.pp cmd stderr stdout
+      );%lwt
+      RunAsync.error "error running command"
+  in
+  try%lwt
+    let cmd = Cmd.getToolAndLine cmd in
+    Lwt_process.with_process_full cmd f
+  with
+  | Unix.Unix_error (err, _, _) ->
+    let msg = Unix.error_message err in
+    RunAsync.error msg
+  | _ ->
+    RunAsync.error "error running subprocess"
+
 let apply ~strip ~root ~patch () =
   let cmd = Cmd.(
     v "patch"
@@ -5,4 +29,4 @@ let apply ~strip ~root ~patch () =
     % "--strip" % string_of_int strip
     % "--input" % p patch
   ) in
-  ChildProcess.run cmd
+  runPatch cmd
