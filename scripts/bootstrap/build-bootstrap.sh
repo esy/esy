161e8ef2d648a79d62c8d760b10f70485df2deec
Bryan Phelps <bryphe@outlook.com>
2018-07-03 18:12:07 -0700

    Add logging for esyi failure

--- scripts/bootstrap/build-bootstrap.sh
+++ scripts/bootstrap/build-bootstrap.sh
@@ -9,15 +9,32 @@ set -o pipefail
 
 cp scripts/bootstrap/Makefile.bootstrap Makefile
 
-make bootstrap
+echo "Bootstrap round 1"
 make bootstrap
 
+echo "Bootstrap round 2"
+make bootstrap
+
+echo "jbuilder:build esy-build-package"
 jbuilder build _build/default/esy-build-package/bin/esyBuildPackageCommand.exe
+
+echo "jbuilder: build esy"
 jbuilder build _build/default/esy/bin/esyCommand.exe
 
+echo "make: esy-install"
 make _release/bin/esy-install.js
+
+echo "make: postinstall.sh"
 make _release/bin/esyInstallRelease.js
+
+echo "make: postinstall.sh"
 make _release/scripts/postinstall.sh
+
+echo "make: package.json"
 make _release/package.json
+
+echo "make: release esy-build-package"
 make _release/_build/default/esy-build-package/bin/esyBuildPackageCommand.exe
+
+echo "make: release esy"
 make _release/_build/default/esy/bin/esyCommand.exe
