ad7ce48dc6a4a76b9ab61a2852721bdaf76f5134
Andrey Popp <8mayday@gmail.com>
2018-07-04 12:39:47 +0300

    esy: cleanup System

--- esy/System.ml
+++ esy/System.ml
@@ -5,10 +5,10 @@ type system =
   | Other
 
 let uname () =
-  let ic = Unix.open_process_in("uname") in
-  let uname = input_line(ic) in
-  let () = close_in(ic) in
-  match String.lowercase_ascii(uname) with
+  let ic = Unix.open_process_in "uname" in
+  let uname = input_line ic in
+  let () = close_in ic in
+  match String.lowercase_ascii uname with
   | "linux" -> Linux
   | "darwin" -> Darwin
   | _ -> Other
