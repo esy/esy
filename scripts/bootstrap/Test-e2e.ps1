54303d6d3bd191053cf56bf5562c8b6fd3458f37
Bryan Phelps <bryphe@outlook.com>
2018-07-10 17:11:56 -0700

    Use relative directory PATH

--- scripts/bootstrap/Test-e2e.ps1
+++ scripts/bootstrap/Test-e2e.ps1
@@ -2,13 +2,17 @@
 #
 # This is a shim to make running the e2e tests work on Windows
 
+function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName }
+
 function exitIfFailed() {
     if ($LastExitCode -ne 0) {
         exit $LastExitCode
     }
 }
 
-cd /esy3/test-e2e
+$endToEndTestFolder = Join-Path (Get-ScriptDirectory) '../../test-e2e'
+
+cd $endToEndTestFolder
 
 yarn install; exitIfFailed;
 
