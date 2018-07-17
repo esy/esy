1678d6d125ed7fc59c57b163a0d1c73cb70b6857
Bryan Phelps <bryphe@outlook.com>
2018-07-17 14:10:01 -0700

    Windows: NPM Release - Set up artifact publishing for Windows builds (#248)
    * Add script to create release tar.gz to be combined with other release for our 'fat' npm package
    
    * Set up appveyor to push windows builds for tag
    
    * Fix issue in appveyor.yml

---
+++ scripts/bootstrap/make-release-package.js
@@ -0,0 +1,45 @@
+const path = require("path")
+const fs = require("fs")
+const os = require("os")
+
+const { bashExec, toCygwinPath } = require("esy-bash")
+
+const rootFolder = path.join(__dirname, "..", "..")
+const packageJson = path.join(rootFolder, "package.json")
+const packFolder = path.join(rootFolder, "_release")
+const destFolder = path.join(rootFolder, "_platformrelease")
+
+const version = require(packageJson).version
+
+const getArch = () => {
+    let arch = os.arch() == ("x32" || "ia32") ? "x86" : "x64"
+
+    if (process.env.APPVEYOR) {
+        arch = process.env.PLATFORM === "x86" ? "x86" : "x64"
+    }
+
+    return arch
+}
+
+const arch = getArch();
+
+const pack = async () => {
+    // If we pack cygwin + all the installed dependencies, the archive by itself
+    // is around 338 MB! If we pack that for both x86 + x64, we'll end up with almost 750 MB.
+    // Doesn't seem acceptable for now. The downside is the user will need to download / install
+    // cygwin as port of a postinstall step (since `esy-bash` is called out as a dependency,
+    // this should happen automatically).
+    console.log("Deleting cygwin from release folder...")
+    await bashExec(`rm -rf ${packFolder}/node_modules/esy-bash`)
+
+    console.log("Creating folder...")
+    await bashExec(`mkdir ${destFolder}`)
+
+    const cygwinDestFolder = await toCygwinPath(destFolder)
+    const cygwinPackFolder = await toCygwinPath(packFolder)
+
+    console.log(`Creating archive from ${cygwinPackFolder} in ${cygwinDestFolder}.`)
+    await bashExec(`tar -czvf ${cygwinDestFolder}/esy-windows-${version}-${arch}.tar.gz -C ${cygwinPackFolder} .`)
+}
+
+pack()
