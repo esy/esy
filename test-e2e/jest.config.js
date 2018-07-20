7012c2697a9c7606d46438407884665b0ef0b6f4
Ulrik Strid <ulrik.strid@outlook.com>
2018-07-20 23:43:29 +0200

    Fix jest.config.js

--- test-e2e/jest.config.js
+++ test-e2e/jest.config.js
@@ -3,13 +3,12 @@ module.exports = {
     {
       displayName: 'e2e - JavaScript',
       moduleFileExtensions: ['js'],
-      runner: 'jest-runner',
       testMatch: ['<rootDir>/**/*.test.js'],
     },
     {
       moduleFileExtensions: ['sh'],
       displayName: 'e2e - sh',
-      runner: './jest-bash-runner/index.js',
+      testRunner: './jest-bash-runner/index.js',
       testMatch: ['<rootDir>/**/*-test.sh'],
     },
   ],
