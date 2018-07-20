e69bad7b61d21e9e2f949fe2add08c97bb41a9bd
Andrey Popp <8mayday@gmail.com>
2018-07-20 13:14:30 +0300

    esyi: cleanup Sandbox

--- esyi/Sandbox.ml
+++ esyi/Sandbox.ml
@@ -7,70 +7,65 @@ type t = {
   ocamlReq : Package.Req.t option;
 }
 
-module EsyManifest = struct
-  module ParseResolutions = struct
-    type t = {
-      resolutions : (Package.Resolutions.t [@default Package.Resolutions.empty]);
-    } [@@deriving of_yojson { strict = false }]
-  end
-
-  let ofDir (path : Path.t) =
-    let open RunAsync.Syntax in
-    match%bind Manifest.find path with
-    | Some filename ->
-      let%bind json = Fs.readJsonFile filename in
-      let%bind pkgJson = RunAsync.ofRun (Json.parseJsonWith Manifest.PackageJson.of_yojson json) in
-      let%bind resolutions = RunAsync.ofRun (Json.parseJsonWith ParseResolutions.of_yojson json) in
-      let manifest = Manifest.ofPackageJson pkgJson in
-      return (Some (manifest, resolutions.ParseResolutions.resolutions))
-    | None -> return None
+module PackageJsonWithResolutions = struct
+  type t = {
+    resolutions : (Package.Resolutions.t [@default Package.Resolutions.empty]);
+  } [@@deriving of_yojson { strict = false }]
 end
 
-module OpamManifest = struct
-  let ofDir (path : Path.t) =
-    let open RunAsync.Syntax in
-    let%bind filenames = Fs.listDir path in
-    let%bind opams =
-      let version = OpamPackage.Version.of_string "dev" in
-      filenames
-      |> List.map ~f:(fun name -> Path.(path / name))
-      |> List.filter ~f:(fun path ->
-          Path.has_ext ".opam" path || Path.basename path = "opam")
-      |> List.map ~f:(fun path ->
-          let name =
-            let name = Path.(path |> rem_ext |> basename) in
-            OpamPackage.Name.of_string name in
-          OpamRegistry.Manifest.ofFile ~name ~version path)
-      |> RunAsync.List.joinAll
+let readPackageJsonManifest (path : Path.t) =
+  let open RunAsync.Syntax in
+  match%bind Manifest.find path with
+  | Some filename ->
+    let%bind json = Fs.readJsonFile filename in
+    let%bind pkgJson = RunAsync.ofRun (Json.parseJsonWith Manifest.PackageJson.of_yojson json) in
+    let%bind resolutions = RunAsync.ofRun (Json.parseJsonWith PackageJsonWithResolutions.of_yojson json) in
+    let manifest = Manifest.ofPackageJson pkgJson in
+    return (Some (manifest, resolutions.PackageJsonWithResolutions.resolutions))
+  | None -> return None
+
+let readAggregatedOpamManifest (path : Path.t) =
+  let open RunAsync.Syntax in
+  let%bind filenames = Fs.listDir path in
+  let%bind opams =
+    let version = OpamPackage.Version.of_string "dev" in
+    filenames
+    |> List.map ~f:(fun name -> Path.(path / name))
+    |> List.filter ~f:(fun path ->
+        Path.has_ext ".opam" path || Path.basename path = "opam")
+    |> List.map ~f:(fun path ->
+        let name =
+          let name = Path.(path |> rem_ext |> basename) in
+          OpamPackage.Name.of_string name in
+        OpamRegistry.Manifest.ofFile ~name ~version path)
+    |> RunAsync.List.joinAll
+  in
+  match opams with
+  | [] -> return None
+  | opams ->
+    let%bind pkgs =
+      let version = Package.Version.Source (Package.Source.LocalPath path) in
+      let f opam = OpamRegistry.Manifest.toPackage ~name:"root" ~version opam in
+      RunAsync.List.joinAll (List.map ~f opams)
     in
-    match opams with
-    | [] -> return None
-    | opams ->
-      let%bind pkgs =
-        let version = Package.Version.Source (Package.Source.LocalPath path) in
-        let f opam = OpamRegistry.Manifest.toPackage ~name:"root" ~version opam in
-        RunAsync.List.joinAll (List.map ~f opams)
+    let dependencies, devDependencies =
+      let f (dependencies, devDependencies) (pkg : Package.t) =
+        match pkg.dependencies, pkg.devDependencies with
+        | Package.Dependencies.OpamFormula du, Package.Dependencies.OpamFormula ddu ->
+          du @ dependencies, ddu @ devDependencies
+        | _ -> assert false
       in
-      let dependencies, devDependencies =
-        let f (dependencies, devDependencies) (pkg : Package.t) =
-          match pkg.dependencies, pkg.devDependencies with
-          | Package.Dependencies.OpamFormula du, Package.Dependencies.OpamFormula ddu ->
-            du @ dependencies, ddu @ devDependencies
-          | _ -> assert false
-        in
-        List.fold_left ~f ~init:([], []) pkgs
-      in
-      return (Some (dependencies, devDependencies))
-end
+      List.fold_left ~f ~init:([], []) pkgs
+    in
+    return (Some (dependencies, devDependencies))
 
 let ocamlReqAny =
-  Package.Req.make ~name:"ocaml" ~spec:"*"
-  |> Run.ofStringError
-  |> Run.runExn ~err:"invalid ocaml constraint"
+  let spec = Package.VersionSpec.Npm SemverVersion.Formula.any in
+  Package.Req.ofSpec ~name:"ocaml" ~spec
 
 let ofDir ~cfg (path : Path.t) =
   let open RunAsync.Syntax in
-  match%bind EsyManifest.ofDir path with
+  match%bind readPackageJsonManifest path with
   | Some (manifest, resolutions) ->
 
     let reqs =
@@ -93,7 +88,7 @@ let ofDir ~cfg (path : Path.t) =
       dependencies = Package.Dependencies.NpmFormula reqs;
     }
   | None ->
-    begin match%bind OpamManifest.ofDir path with
+    begin match%bind readAggregatedOpamManifest path with
     | Some (dependencies, devDependencies) ->
 
       let root = {
--- esyi/Sandbox.mli
+++ esyi/Sandbox.mli
@@ -10,7 +10,12 @@ type t = {
   (** Root package. *)
   root : Package.t;
 
-  (** A set of dependencies to be installed for the sandbox. *)
+  (**
+   * A set of dependencies to be installed for the sandbox.
+   *
+   * Such dependencies are different than of root.dependencies as sandbox
+   * aggregates both regular dependencies and devDependencies.
+   *)
   dependencies : Package.Dependencies.t;
 
   (** A set of resolutions. *)
