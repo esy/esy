b164dc61e7036ffd1419902d0f2ae9b89b62e114
Andrey Popp <8mayday@gmail.com>
2018-07-25 11:41:16 +0300

    tests: remove jest-bash-runner

--- jest.config.js
+++ jest.config.js
@@ -4,12 +4,6 @@ module.exports = {
       displayName: 'e2e:js',
       moduleFileExtensions: ['js'],
       testMatch: ['<rootDir>/test-e2e/**/*.test.js'],
-    },
-    {
-      moduleFileExtensions: ['sh', 'js'],
-      displayName: 'e2e:sh',
-      testRunner: '<rootDir>/jest-bash-runner/index.js',
-      testMatch: ['<rootDir>/test-e2e/**/*-test.sh'],
-    },
+    }
   ],
 };
