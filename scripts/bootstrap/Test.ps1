4f22bb1f21f41055a3d53c507f8dd78a508799a6
Bryan Phelps <bryphe@outlook.com>
2018-07-26 16:29:58 -0700

    @slowtest: Hook up slowtests

--- scripts/bootstrap/Test.ps1
+++ scripts/bootstrap/Test.ps1
@@ -20,14 +20,35 @@ ls
 mkdir C:/esy-home
 $env:HOME="C:/esy-home"
 
-# "Integration Test" for now
-# This requires retries on all platforms at the moment:
-C:/projects/esy/_release/_build/default/esy/bin/esyCommand.exe legacy-install
-C:/projects/esy/_release/_build/default/esy/bin/esyCommand.exe legacy-install
-C:/projects/esy/_release/_build/default/esy/bin/esyCommand.exe legacy-install
 
-exitIfFailed
+# Install esy's dependencies so that we can run the jest tests
 
-# TODO: Bring this back when we have a project that can build successfully!
-# C:/projects/esy/_release/_build/default/esy/bin/esyCommand.exe build
-# exitIfFailed
+cd C:/projects/esy
+
+C:/projects/esy/_release/_build/default/esy/bin/esyCommand.exe legacy-install
+C:/projects/esy/_release/_build/default/esy/bin/esyCommand.exe legacy-install
+C:/projects/esy/_release/_build/default/esy/bin/esyCommand.exe legacy-install
+ exitIfFailed
+
+ npm run test-e2e
+ exitIfFailed
+
+# Run slow-e2e tests if necessary
+
+$shouldRunTest = 0
+if ($env:APPVEYOR_REPO_COMMIT_MESSAGE) {
+    $shouldRunTest = $env:APPVEYOR_REPO_COMMIT_MESSAGE.Contains("@slowtest")
+}
+
+if ($shouldRunTest) {
+    Write-Host "Running slow tests..."
+
+    # Not passing yet on Windows, hold off on this one
+    # node test-e2e/build-top-100-opam.slowtest.js
+    # exitIfFailed
+
+    node test-e2e/install-npm.slowtest.js
+    exitIfFailed
+} else {
+    Write-Host "Skipped slow tests."
+}
