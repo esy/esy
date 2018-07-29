f061981b89024778ca993a9e911697458254df7a
Bryan Phelps <bryphe@outlook.com>
2018-07-29 14:31:15 -0700

    Add test environment to suppress security error

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   displayName: 'e2e:fast',
   moduleFileExtensions: ['js'],
   testMatch: ['<rootDir>/test-e2e/**/*.test.js'],
+  testEnvironment: "node",
   modulePathIgnorePatterns: [
     '<rootDir>/esy-install/',
     '<rootDir>/node_modules/',
