c26f8397c226474b6d6f6a15e80a43ff2a142c89
Andrey Popp <8mayday@gmail.com>
2018-07-30 19:33:44 +0300

    tests: fix custom-prefix test

--- test-e2e/build/custom-prefix.test.js
+++ test-e2e/build/custom-prefix.test.js
@@ -9,33 +9,18 @@ const fixture = [
   packageJson({
     "name": "custom-prefix",
     "version": "1.0.0",
-    "license": "MIT",
     "esy": {
       "build": [
-        [
-          "cp",
-          "#{self.original_root /}test.ml",
-          "#{self.target_dir /}test.ml"
-        ],
-        [
-          "ocamlopt",
-          "-o",
-          "#{self.target_dir / self.name}.exe",
-          "#{self.target_dir /}test.ml"
-        ]
+        "cp #{self.root /}test.ml #{self.target_dir /}test.ml",
+        "ocamlopt -o #{self.target_dir / self.name}.exe #{self.target_dir /}test.ml"
       ],
-      "install": [
-        [
-          "cp",
-          "#{self.target_dir / self.name}.exe",
-          "#{self.bin / self.name}"
-        ]
-      ]
+      "install": "cp #{self.target_dir / self.name}.exe #{self.bin / self.name}"
     },
     "dependencies": {
       "ocaml": "*"
     }
   }),
+  file('.esyrc', 'esy-prefix-path: ./store'),
   file('test.ml', outdent`
     let () = print_endline "custom-prefix"
   `),
@@ -45,11 +30,8 @@ const fixture = [
 ];
 
 it('Build - custom prefix', async () => {
-  jest.setTimeout(200000);
-  expect.assertions(1);
   const p = await genFixture(...fixture);
 
-  await p.esy('install', {noEsyPrefix: true});
   await p.esy('build', {noEsyPrefix: true});
 
   const {stdout} = await p.esy('x custom-prefix', {noEsyPrefix: true});
