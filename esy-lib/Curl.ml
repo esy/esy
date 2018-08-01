9ff7718797183a63c0de0b60a1cac2383cd9c2fa
Bryan Phelps <bryphe@outlook.com>
2018-08-01 14:14:27 -0700

    Revert Curl changes

--- esy-lib/Curl.ml
+++ esy-lib/Curl.ml
@@ -60,7 +60,8 @@ let runCurl cmd =
     end
   in
   try%lwt
-    EsyBashLwt.with_process_full cmd f
+    let cmd = Cmd.getToolAndLine cmd in
+    Lwt_process.with_process_full cmd f
   with
   | Unix.Unix_error (err, _, _) ->
     let msg = Unix.error_message err in
