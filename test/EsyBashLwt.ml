503233d80cfe9e9cbc034c3b68a69288c4a7b810
Bryan Phelps <bryphe@outlook.com>
2018-07-31 22:51:39 -0700

    Stub out tests for EsyBashLwt

---
+++ test/EsyBashLwt.ml
@@ -0,0 +1,11 @@
+include EsyLib.Curl
+
+module Fs = EsyLib.Fs
+module Path = EsyLib.Path
+module RunAsync = EsyLib.RunAsync
+
+let%test "EsyBashLwt runs a simple bash command" =
+    false
+
+let%test "EsyBashLwt preseves exit code" =
+    false
