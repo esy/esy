779115ca673300a3f0aa1833a6a5acddf18cbb36
Bryan Phelps <bryphe@outlook.com>
2018-08-01 17:24:35 -0700

    Clean up Curl test

--- test/Curl.ml
+++ test/Curl.ml
@@ -40,15 +40,9 @@ let%test "curl download simple file" =
 let%test "curl gives error when failing to download" =
     let test () = 
         let f tempPath =
-            (* use curl to copy the file, as opposed to hitting an external server *)
             let output = Path.(tempPath / "output.txt") in
-
-            (* We need to normalize the path on Windows - file:///E:/.../ won't work! *)
-            (* The normalize gives us a path of the form file:///cygdrive/e/.../ which does. *)
-            (* This won't impact HTTP requests though - just our test using the local file system *)
             let url = "file:///some/nonexistent/file" in
             let%lwt result = EsyLib.Curl.download ~output url in
-
             match result with
             | Error _ -> Lwt.return true
             | _ -> Lwt.return false
