f1bb41ed79ca638020ea5b8be90964c9aae7b26b
Bryan Phelps <bryphe@outlook.com>
2018-08-02 14:06:28 -0700

    Add test for stripComponents

--- test/Tarball.ml
+++ test/Tarball.ml
@@ -31,3 +31,33 @@ let%test "creates and unpacks a tarball" =
         Fs.withTempDir f
     in
     TestLwt.runLwtTest test
+
+let%test "unpack tarball with stripcomponents" =
+    let test () = 
+        let f tempPath =
+            let folderToCreate = Path.(tempPath / "test-folder" / "nested-folder-1" / "nested-folder-2") in
+            let%lwt _ = Fs.createDir folderToCreate in
+            let fileToCreate = Path.(folderToCreate / "test-file.txt") in
+            let data = "test data" in
+            let%lwt _ = Fs.writeFile ~data fileToCreate in
+
+            (* package up the file into a tarball *)
+            let folderToPackage = Path.(tempPath / "test-folder") in
+            let filename = Path.(tempPath / "output.tar.gz") in
+            let%lwt _ = EsyLib.Tarball.create ~filename folderToPackage in
+
+            (* unpack the tarball *)
+            let dst = Path.(tempPath / "extract-folder") in
+            let%lwt _ = Fs.createDir dst in 
+            let stripComponents = 2 in
+            let%lwt _ = EsyLib.Tarball.unpack ~stripComponents ~dst filename in
+
+            let expectedOutputFile = Path.(dst / "test-file.txt") in
+            let%lwt result = Fs.readFile expectedOutputFile in
+            match result with 
+            | Ok v -> Lwt.return (v = data)
+            | _ -> Lwt.return false
+        in
+        Fs.withTempDir f
+    in
+    TestLwt.runLwtTest test
