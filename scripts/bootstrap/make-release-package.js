98edf605a52e77ed2a054eda2d47a7fefb5490de
Bryan Phelps <bryphe@outlook.com>
2018-08-02 03:16:18 -0700

    Windows: Release package fixes (#323)
    * Add additional logging to make-release-package
    
    * Throw on non-zero exit code from bash

--- scripts/bootstrap/make-release-package.js
+++ scripts/bootstrap/make-release-package.js
@@ -23,6 +23,14 @@ const getArch = () => {
 
 const arch = getArch();
 
+const bashExecAndThrow = async (command) => {
+    const code = await bashExec(command)
+
+    if (code !== 0) {
+        throw new Error(`Command: ${command} failed with exit code ${code}`)
+    }
+}
+
 const pack = async () => {
     // If we pack cygwin + all the installed dependencies, the archive by itself
     // is around 338 MB! If we pack that for both x86 + x64, we'll end up with almost 750 MB.
@@ -30,16 +38,18 @@ const pack = async () => {
     // cygwin as port of a postinstall step (since `esy-bash` is called out as a dependency,
     // this should happen automatically).
     console.log("Deleting cygwin from release folder...")
-    await bashExec(`rm -rf ${packFolder}/node_modules/esy-bash`)
-
-    console.log("Creating folder...")
-    await bashExec(`mkdir ${destFolder}`)
 
     const cygwinDestFolder = await toCygwinPath(destFolder)
     const cygwinPackFolder = await toCygwinPath(packFolder)
 
+    console.log(`- Deleting: ${cygwinPackFolder}/node_modules/esy-bash`)
+    await bashExecAndThrow(`rm -rf ${cygwinPackFolder}/node_modules/esy-bash`)
+
+    console.log(`Creating folder: ${cygwinDestFolder}`)
+    await bashExecAndThrow(`mkdir ${cygwinDestFolder}`)
+
     console.log(`Creating archive from ${cygwinPackFolder} in ${cygwinDestFolder}.`)
-    await bashExec(`tar -czvf ${cygwinDestFolder}/esy-v${version}-windows-${arch}.tgz -C ${cygwinPackFolder} .`)
+    await bashExecAndThrow(`tar -czvf ${cygwinDestFolder}/esy-v${version}-windows-${arch}.tgz -C ${cygwinPackFolder} .`)
 }
 
 pack()
