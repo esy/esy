3a8530f28a8812295bef524e64e11788ab904b44
Andrey Popp <8mayday@gmail.com>
2018-08-07 14:28:55 +0300

    esyi/OpamRegistry docstrings

--- esyi/OpamRegistry.mli
+++ esyi/OpamRegistry.mli
@@ -1,6 +1,10 @@
+(**
+ * API for querying opam registry.
+ *)
+
 type t
 
-type resolution = {
+type resolution = private {
   name: OpamPackage.Name.t;
   version: OpamPackage.Version.t;
   opam: Path.t;
@@ -24,15 +28,18 @@ module Manifest : sig
 end
 
 val make : cfg:Config.t -> unit -> t
+(** Configure a new opam registry instance. *)
 
 val versions :
   ?ocamlVersion:OpamVersion.Version.t
   -> name : OpamPackage.Name.t
   -> t
   -> resolution list RunAsync.t
+(** Return a list of resolutions for a given opam package name. *)
 
 val version :
-    name : OpamPackage.Name.t
-    -> version : OpamPackage.Version.t
-    -> t
-    -> Manifest.t option RunAsync.t
+  name : OpamPackage.Name.t
+  -> version : OpamPackage.Version.t
+  -> t
+  -> Manifest.t option RunAsync.t
+(** Return an opam manifest for a given opam package name, version. *)
