0712c418aa2455937dd3ef39bf80caef3a8ddf71
Andrey Popp <8mayday@gmail.com>
2018-08-22 12:33:48 +0300

    tests: more comprehensive tests for different env

--- test-e2e/common/fixture.js
+++ test-e2e/common/fixture.js
@@ -12,7 +12,15 @@ const simpleProject = [
     devDependencies: {
       devDep: '*',
     },
-    esy: {},
+    esy: {
+      buildEnv: {
+        root__build: 'root__build__value',
+      },
+      exportedEnv: {
+        root__local: {val: 'root__local__value'},
+        root__global: {val: 'root__global__value', scope: 'global'},
+      },
+    },
   }),
   dir(
     'node_modules',
@@ -25,13 +33,32 @@ const simpleProject = [
           buildsInSource: true,
           build: 'ocamlopt -o #{self.root / self.name} #{self.root / self.name}.ml',
           install: 'cp #{self.root / self.name} #{self.bin / self.name}',
+          exportedEnv: {
+            dep__local: {val: 'dep__local__value'},
+            dep__global: {val: 'dep__global__value', scope: 'global'},
+          },
         },
         dependencies: {
           ocaml: '*',
+          depOfDep: '*',
         },
         _resolved: '...',
       }),
       file('dep.ml', 'let () = print_endline "__dep__"'),
+    ),
+    dir(
+      'depOfDep',
+      packageJson({
+        name: 'depOfDep',
+        version: '1.0.0',
+        esy: {
+          exportedEnv: {
+            depOfDep__local: {val: 'depOfDep__local__value'},
+            depOfDep__global: {val: 'depOfDep__global__value', scope: 'global'},
+          },
+        },
+        _resolved: '...',
+      }),
     ),
     dir(
       'devDep',
@@ -42,6 +69,10 @@ const simpleProject = [
           buildsInSource: true,
           build: 'ocamlopt -o #{self.root / self.name} #{self.root / self.name}.ml',
           install: 'cp #{self.root / self.name} #{self.bin / self.name}',
+          exportedEnv: {
+            devDep__local: {val: 'devDep__local__value'},
+            devDep__global: {val: 'devDep__global__value', scope: 'global'},
+          },
         },
         dependencies: {
           ocaml: '*',
