1f8e4b5a9dd18a1c9605c684a99864f7c7858695
Andrey Popp <8mayday@gmail.com>
2018-08-23 17:55:56 +0300

    Remove esy-install dir

--- esy-install/src/lib/lang.js
+++
@@ -1,8 +0,0 @@
-/**
- * @flow
- */
-
-export function fixupErrorSubclassing(instance: any, constructor: Function) {
-  instance.constructor = constructor;
-  instance.__proto__ = constructor.prototype;
-}
