4c15b85dc8de088ffcaf1effe315e19921f3d816
Ulrik Strid <ulrik.strid@outlook.com>
2018-09-14 21:21:49 +0200

    Initial implementation of pure ocaml untar

--- esy-lib/Tarball.ml
+++ esy-lib/Tarball.ml
@@ -43,17 +43,61 @@ let run cmd =
   | _ ->
     RunAsync.error "error running subprocess"
 
+
 let unpackWithTar ?stripComponents ~dst filename =
   let open RunAsync.Syntax in
   let unpack out = 
-    let%bind cmd = RunAsync.ofBosError (
-      let open Result.Syntax in
-      let%bind nf = EsyBash.normalizePathForCygwin (Path.show filename) in
-      let%bind normalizedOut = EsyBash.normalizePathForCygwin (Path.show out) in
-      return Cmd.(v "tar" % "xf" % nf % "-C" % normalizedOut)
-    )
+    let nf = Path.toString filename in
+    let _normalizedOut = Path.toString out in
+
+    let max_ocaml_int = Int64.of_int max_int in
+
+    let readFile input_channel (header : Tar_cstruct.Header.t) =
+      let file_size = header.file_size in
+      (* If this were to happen we'd have some pretty big problems... *)
+      assert (file_size <= max_ocaml_int) ;
+      let buf = Cstruct.create (Int64.to_int file_size) in
+      Tar_cstruct.really_read input_channel buf ;
+
+      let nameLength = String.length header.file_name in
+      let maxLength = nameLength - 2 in
+
+      let filename = Filename.concat (Path.toString out) (String.sub header.file_name 2 maxLength) in
+
+      if filename = "" then
+        RunAsync.return ()
+      else 
+        let filepath = match Path.ofString filename with
+        | Ok path -> path
+        | Error `Msg message -> failwith message
+        in
+        let%bind () = match Cstruct.to_string buf with
+        | "" -> Fs.createDir filepath
+        | data -> Fs.writeFile ~data filepath
+        in
+        RunAsync.return ()
     in
-    run cmd
+
+    let rec readFiles ic =
+      match Tar_cstruct.Archive.with_next_file ic readFile with
+      | exception Tar_cstruct.Header.End_of_stream -> ()
+      | _file -> readFiles ic
+    in
+
+    try
+      let ic = open_in_bin nf in
+      let length = in_channel_length ic in
+      let rawTarGz = really_input_string ic length in
+
+      let rawTarFile = match Ezgzip.decompress rawTarGz with
+      | (Ok tar) -> tar
+      | (Error _) -> (failwith "Invalid gzip")
+      in
+
+      let tarFile = Tar_cstruct.make_in_channel (Cstruct.of_string rawTarFile) in
+
+      RunAsync.return (readFiles tarFile)
+    with Sys_error message -> RunAsync.error message
   in
   match stripComponents with
   | Some stripComponents ->
