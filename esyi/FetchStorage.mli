94b55a70ca6d51300ddaec3c6a17cc5c3557c914
Andrey Popp <8mayday@gmail.com>
2018-09-19 15:22:07 +0500

    Expose FetchStorage.fetchSource
    This allows to fetch source into cache. The idea is that we sometimes
    need to fetch source during resolution too. With this function we can
    share cache for sources fetched during resolution and during
    installation.

--- esyi/FetchStorage.mli
+++ esyi/FetchStorage.mli
@@ -2,25 +2,32 @@
  * Package storage.
  *)
 
+val fetchSource :
+  cfg : Config.t
+  -> Source.t
+  -> Path.t RunAsync.t
+(** Fetch source and cache a compressed tarball. *)
+
+(** Distribution. *)
 module Dist : sig
   type t
   val pp : Format.formatter -> t -> unit
 end
 
-(**
- * Make sure package specified by [name], [version] and [source] is in store and
- * return it.
- *)
 val fetch :
   cfg : Config.t
   -> Solution.Record.t
   -> Dist.t RunAsync.t
-
 (**
- * Install package from storage into destination.
+ * Make sure package specified by [name], [version] and [source] is in store and
+ * return it.
  *)
+
 val install :
   cfg : Config.t
   -> path : Path.t
   -> Dist.t
   -> unit RunAsync.t
+(**
+ * Install package from storage into destination.
+ *)
