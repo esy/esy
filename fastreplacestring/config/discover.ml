1a2f20b80c20e5a0daaaec3e75ccb5299ce3583f
Andrey Popp <8mayday@gmail.com>
2018-10-01 19:41:06 +0300

    Fix cxx_flags

--- fastreplacestring/config/discover.ml
+++ fastreplacestring/config/discover.ml
@@ -5,24 +5,13 @@ let () =
 
     let flags =
       match C.ocaml_config_var c "system" with
-      (* do not link statically on macos *)
-      | Some "macosx" -> [
-          "-ccopt"; "-Ofast";
-          "-ccopt"; "-lstdc++";
-        ]
       | Some "mingw64" -> [
           "-ccopt"; "-lstdc++";
-          "-ccopt"; "-link -static";
-          "-ccopt"; "-link -static-libgcc";
-          "-ccopt"; "-link -static-libstdc++";
         ]
       | Some _
       | None -> [
           "-ccopt"; "-Ofast";
           "-ccopt"; "-lstdc++";
-          "-ccopt"; "-link -static";
-          "-ccopt"; "-link -static-libgcc";
-          "-ccopt"; "-link -static-libstdc++";
         ]
     in
     C.Flags.write_sexp "dune.flags" flags;
@@ -30,19 +19,14 @@ let () =
     let cxx_flags =
       match C.ocaml_config_var c "system" with
       (* do not link statically on macos *)
-      | Some "macosx" -> [
+      | Some "mingw64" -> [
         "-lstdc++";
         "-fno-exceptions";
         "-fno-rtti";
         ]
       | Some _
       | None -> [
-        "-lstdc++";
-        "-fno-exceptions";
-        "-fno-rtti";
-        "-static";
-        "-static-libgcc";
-        "-static-libstdc++";
+          "-lstdc++";
         ]
     in
     C.Flags.write_sexp "dune.cxx_flags" cxx_flags
