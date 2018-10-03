561a38dd5957e0a98f341a52e0f18fa9c9b591a2
Bryan Phelps <bryphe@outlook.com>
2018-10-03 13:47:12 -0700

    Run additional tests

--- test-e2e-slow/run-slow-tests.js
+++ test-e2e-slow/run-slow-tests.js
@@ -7,5 +7,8 @@ const latestCommit = execSync("git log --oneline -n1").toString("utf8");
 // run slow tests
 // NOMERGE: Always run slow tests in this branch
 console.log("Running test suite: e2e (slow tests)");
+
 require("./build-top-100-opam.test.js");
 require("./install-npm.test.js");
+require("./esy.test.js");
+require("./reason.test.js");
