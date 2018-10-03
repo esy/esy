85f31287f229256188d531caec0b065b92435b49
Bryan Phelps <bryphe@outlook.com>
2018-10-03 14:19:51 -0700

    See what tests we can enable

--- test-e2e-slow/run-slow-tests.js
+++ test-e2e-slow/run-slow-tests.js
@@ -8,7 +8,11 @@ const latestCommit = execSync("git log --oneline -n1").toString("utf8");
 // NOMERGE: Always run slow tests in this branch
 console.log("Running test suite: e2e (slow tests)");
 
-require("./build-top-100-opam.test.js");
-require("./install-npm.test.js");
-require("./esy.test.js");
+// INVESTIGATE
+// require("./install-npm.test.js");
 require("./reason.test.js");
+
+require("./esy.test.js");
+
+require("./build-top-100-opam.test.js");
+
