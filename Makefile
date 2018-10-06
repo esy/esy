bdcfdf97d8ad3cf6f4c83493c3df01f25e8a0003
Bryan Phelps <bryphe@outlook.com>
2018-10-05 17:07:53 -0700

    @slowtest - refactor to use common script

--- Makefile
+++ Makefile
@@ -119,14 +119,7 @@ test-e2e::
 	@$(BIN)/jest test-e2e
 
 test-e2e-slow::
-	@echo "Running test suite: e2e (slow tests)"
-	@node ./test-e2e-slow/build-top-100-opam.test.js
-	@node ./test-e2e-slow/install-npm.test.js
-	@node ./test-e2e-slow/esy.test.js
-	@node ./test-e2e-slow/reason.test.js
-	@node ./test-e2e-slow/repromise.test.js
-	@node ./test-e2e-slow/fastpack.test.js
-	@node ./test-e2e-slow/release.test.js
+	@node ./test-e2e-slow/run-slow-tests
 
 test::
 	@echo "Running test suite: unit tests"
@@ -136,9 +129,7 @@ test::
 
 ci::
 	@$(MAKE) test
-	@if [ "$$TRAVIS_TAG" != "" ] || [ $$(echo "$$TRAVIS_COMMIT_MESSAGE" | grep "@slowtest") ]; then \
-		$(MAKE) test-e2e-slow; \
-	fi
+	@$(MAKE) test-e2e-slow
 
 #
 # Release
--- test-e2e-slow/run-slow-tests.js
+++ test-e2e-slow/run-slow-tests.js
@@ -5,17 +5,28 @@ const { execSync } = require("child_process");
 const latestCommit = execSync("git log --oneline -n1").toString("utf8");
 
 if (latestCommit.indexOf("@slowtest") === -1 && !process.env["ESY_SLOWTEST"]) {
-    console.log("Not running slowtests.");
+    console.warn("Not running slowtests.");
     process.exit(0);
 }
 
-console.log("-- Running test suite: e2e (slow tests) --");
+const isWindows = os.platform() === "win32";
+
+console.log("Running test suite: e2e (slow tests)");
 
 require("./build-top-100-opam.test.js");
+require("./install-npm.test.js");
 require("./esy.test.js");
 
-// Reason test blocked by: https://github.com/facebook/reason/pull/2209
-// require("./reason.test.js");
+if (!isWindows) {
+    // Reason test blocked by: https://github.com/facebook/reason/pull/2209
+    require("./reason.test.js");
 
-// Needs investigation:
-require("./install-npm.test.js");
+    // Windows: Needs investigation
+    require("./repromise.test.js");
+
+    // Windows: Fastpack build not supported yet.
+    require("./fastpack.test.js");
+
+    // Windows: Release blocked by #418
+    require("./release.test.js");
+}
