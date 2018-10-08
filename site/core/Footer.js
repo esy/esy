e3dd9f082d0f10db40a7d51c4029f66fa4342199
Andrey Popp <8mayday@gmail.com>
2018-10-08 12:07:35 +0300

    docs: disable pjax
    It's buggy with right nav bar and also isn't needed much as pages are
    light.

--- site/core/Footer.js
+++ site/core/Footer.js
@@ -7,7 +7,7 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
-const React = require("react");
+const React = require('react');
 
 const highlighterCode = `
 function fn() {
@@ -25,30 +25,6 @@ if (document.attachEvent ? document.readyState === "complete" : document.readySt
 
 class Footer extends React.Component {
   render() {
-    return (
-      <span>
-        <script src={this.props.config.baseUrl + 'js/redirectBlog.js'}></script>
-        <script src={this.props.config.baseUrl + 'js/pjax-api.js'}></script>
-        <script dangerouslySetInnerHTML={{__html: `window.foo = new Pjax({
-          areas: [
-            // try to use the first query.
-            '.mainContainer, .docsNavContainer .toc .navWrapper',
-            // fallback
-            'body'
-          ],
-          link: '.docsNavContainer:not(.docsSliderActive) a',
-          update: {
-            script: false,
-          }
-        });
-        var languagesMenuItemCopy = document.getElementById("languages-menu");
-        languagesMenuItemCopy.addEventListener("click", function(e){
-          e.preventDefault();
-          e.stopPropagation();
-          e.stopImmediatePropagation();
-        });`}}></script>
-      </span>
-    );
     const currentYear = new Date().getFullYear();
     return (
       <footer className="nav-footer" id="footer">
@@ -66,53 +42,12 @@ class Footer extends React.Component {
             <a
               href={
                 this.props.config.baseUrl +
-                "docs/" +
+                'docs/' +
                 this.props.language +
-                "/getting-started.html"
-              }
-            >
+                '/getting-started.html'
+              }>
               Getting Started
             </a>
-            <a
-              href={
-                this.props.config.baseUrl +
-                "docs/" +
-                this.props.language +
-                "/simple.html"
-              }
-            >
-              Examples
-            </a>
-            <a
-              href={
-                this.props.config.baseUrl +
-                "docs/" +
-                this.props.language +
-                "/common-errors.html"
-              }
-            >
-              FAQ
-            </a>
-          </div>
-          <div>
-            <h5>Community</h5>
-            <a href="https://discord.gg/reasonml" target="_blank">
-              Discord
-            </a>
-            <a href="https://twitter.com/reasonml" target="_blank">
-              Twitter
-            </a>
-            <a
-              href="https://stackoverflow.com/questions/tagged/reason-react"
-              target="_blank"
-            >
-              Stack Overflow
-            </a>
-          </div>
-          <div>
-            <h5>More</h5>
-            <a href={this.props.config.baseUrl + "blog"}>Blog</a>
-            <a href="https://github.com/reasonml/reason-react">GitHub</a>
           </div>
         </section>
       </footer>
