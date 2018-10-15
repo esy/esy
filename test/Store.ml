7829af3b98d7d40b72c003c0ac1e1abcd71cf180
Ulrik Strid <ulrik.strid@outlook.com>
2018-10-15 21:45:15 +0200

    readd store padding @slowtest

--- test/Store.ml
+++ test/Store.ml
@@ -2,16 +2,9 @@ include EsyLib.Store
 
 module System = EsyLib.System
 
-let%test "Validate padding length on Windows is always 1" = 
+let%test "Validate padding length on all platforms is not 1" =
     let prefixPath = Fpath.v "test" in
-    let padding = getPadding ~system:System.Platform.Windows prefixPath in
-    match padding with 
-        | Ok("_") -> true
-        | _ -> false
-
-let%test "Validate padding length on other platforms is not 1" =
-    let prefixPath = Fpath.v "test" in
-    let padding = getPadding ~system:System.Platform.Darwin prefixPath in
+    let padding = getPadding prefixPath in
     match padding with 
         | Ok("_") -> false
         | Error(_) -> false
@@ -20,7 +13,7 @@ let%test "Validate padding length on other platforms is not 1" =
 let%test "Validate an error is given if the path is too long" =
     let superLongPath = String.make 260 'a' in
     let prefixPath = Fpath.v superLongPath in
-    let padding = getPadding ~system:System.Platform.Darwin prefixPath in
+    let padding = getPadding prefixPath in
     match padding with
         | Error(_) -> true
         | _ -> false
