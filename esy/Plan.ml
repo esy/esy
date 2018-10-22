027b17c3a7f820785af7ba6f8e7628639c310c2d
Andrey Popp <8mayday@gmail.com>
2018-10-22 13:36:48 +0300

    Move bin linking and lifecycle execution to FetchStorage
    That way we can minimze the API of FetchStorage and better encapsulate
    installation logic. We still require that packages are being installed
    in a topo sort order (dependencies are installed before the package is
    beign installed as package lifecycle might require dependencies).

--- esy/Plan.ml
+++ esy/Plan.ml
@@ -686,30 +686,11 @@ let execEnv _sandbox plan task =
   |> Scope.env ~includeBuildEnv:false
 
 let rewritePrefix ~cfg ~origPrefix ~destPrefix rootPath =
-  let open RunAsync.Syntax in
-  let rewritePrefixInFile path =
-    let cmd = Cmd.(cfg.Config.fastreplacestringCmd % p path % p origPrefix % p destPrefix) in
-    ChildProcess.run cmd
-  in
-  let rewriteTargetInSymlink path =
-    let%bind link = Fs.readlink path in
-    match Path.remPrefix origPrefix link with
-    | Some basePath ->
-      let nextTargetPath = Path.(destPrefix // basePath) in
-      let%bind () = Fs.unlink path in
-      let%bind () = Fs.symlink ~src:nextTargetPath path in
-      return ()
-    | None -> return ()
-  in
-  let rewrite (path : Path.t) (stats : Unix.stats) =
-    match stats.st_kind with
-    | Unix.S_REG ->
-      rewritePrefixInFile path
-    | Unix.S_LNK ->
-      rewriteTargetInSymlink path
-    | _ -> return ()
-  in
-  Fs.traverse ~f:rewrite rootPath
+  Fastreplacestring.rewritePrefix
+    ~fastreplacestringCmd:cfg.Config.fastreplacestringCmd
+    ~origPrefix
+    ~destPrefix
+    rootPath
 
 let exportBuild ~cfg ~outputPrefixPath buildPath =
   let open RunAsync.Syntax in
--- esyi/Fetch.ml
+++ esyi/Fetch.ml
@@ -5,180 +5,6 @@ module Dist = FetchStorage.Dist
 let nodeCmd =
   Cmd.resolveCmd System.Environment.path "node"
 
-module PackageJson = struct
-
-  module Scripts = struct
-    type t = {
-      postinstall : (string option [@default None]);
-      install : (string option [@default None]);
-    }
-    [@@deriving of_yojson { strict = false }]
-
-    let empty = {postinstall = None; install = None}
-  end
-
-  module Bin = struct
-    type t =
-      | Empty
-      | One of string
-      | Many of string StringMap.t
-
-    let of_yojson =
-      let open Result.Syntax in
-      function
-      | `String cmd ->
-        let cmd = String.trim cmd in
-        begin match cmd with
-        | "" -> return Empty
-        | cmd -> return (One cmd)
-        end
-      | `Assoc items ->
-        let%bind items =
-          let f cmds (name, json) =
-            match json with
-            | `String cmd -> return (StringMap.add name cmd cmds)
-            | _ -> error "expected a string"
-          in
-          Result.List.foldLeft ~f ~init:StringMap.empty items
-        in
-        return (Many items)
-      | _ -> error "expected a string or an object"
-  end
-
-  type t = {
-    name : string option [@default None];
-    bin : (Bin.t [@default Bin.Empty]);
-    scripts : (Scripts.t [@default Scripts.empty]);
-    esy : (Json.t option [@default None]);
-  } [@@deriving of_yojson { strict = false }]
-
-  let ofDir path =
-    let open RunAsync.Syntax in
-    let filename = Path.(path / "package.json") in
-    if%bind Fs.exists filename
-    then
-      let%bind json = Fs.readJsonFile filename in
-      let%bind manifest = RunAsync.ofRun (Json.parseJsonWith of_yojson json) in
-      return (Some manifest)
-    else
-      return None
-
-  let packageCommands (sourcePath : Path.t) manifest =
-    let makePathToCmd cmdPath = Path.(sourcePath // v cmdPath |> normalize) in
-    match manifest.bin, manifest.name with
-    | Bin.One cmd, Some name ->
-      [name, makePathToCmd cmd]
-    | Bin.One cmd, None ->
-      let cmd = makePathToCmd cmd in
-      let name = Path.basename cmd in
-      [name, cmd]
-    | Bin.Many cmds, _ ->
-      let f name cmd cmds = (name, makePathToCmd cmd)::cmds in
-      (StringMap.fold f cmds [])
-    | Bin.Empty, _ -> []
-
-end
-
-module Install = struct
-
-  type t = {
-    path : Path.t;
-    sourcePath : Path.t;
-    pkg : Package.t;
-    isDirectDependencyOfRoot : bool;
-    status: FetchStorage.status;
-  }
-
-  let pp fmt installation =
-    Fmt.pf fmt "%a at %a"
-      Package.pp installation.pkg
-      Path.pp installation.path
-end
-
-let runLifecycleScript ?env ~install ~lifecycleName script =
-  let%lwt () = Logs_lwt.debug
-    (fun m ->
-      m "Fetch.runLifecycleScript ~env:%a ~pkg:%a ~lifecycleName:%s"
-      (Fmt.option ChildProcess.pp_env) env
-      Package.pp install.Install.pkg
-      lifecycleName
-    )
-  in
-
-  let%lwt () = Logs_lwt.app
-    (fun m ->
-      m "%a: running %a lifecycle"
-      Package.pp install.Install.pkg
-      Fmt.(styled `Bold string) lifecycleName
-    )
-  in
-
-  let readAndCloseChan ic =
-    Lwt.finalize
-      (fun () -> Lwt_io.read ic)
-      (fun () -> Lwt_io.close ic)
-  in
-
-  let f p =
-    let%lwt stdout = readAndCloseChan p#stdout
-    and stderr = readAndCloseChan p#stderr in
-    match%lwt p#status with
-    | Unix.WEXITED 0 ->
-      RunAsync.return ()
-    | _ ->
-      Logs_lwt.err (fun m -> m
-        "@[<v>command failed: %s@\nstderr:@[<v 2>@\n%s@]@\nstdout:@[<v 2>@\n%s@]@]"
-        script stderr stdout
-      );%lwt
-      RunAsync.error "error running command"
-  in
-
-  try%lwt
-    (* We don't need to wrap the install path on Windows in quotes *)
-    let installationPath =
-      match System.Platform.host with
-      | Windows -> Path.show install.path
-      | _ -> Filename.quote (Path.show install.path)
-    in
-    let script =
-      Printf.sprintf
-        "cd %s && %s"
-        installationPath
-        script
-    in
-    let cmd =
-      match System.Platform.host with
-      | Windows -> ("", [|"cmd.exe";("/c " ^ script)|])
-      | _ -> ("/bin/bash", [|"/bin/bash";"-c";script|])
-    in
-    let env =
-      let open Option.Syntax in
-      let%bind env = env in
-      let%bind _, env = ChildProcess.prepareEnv env in
-      return env
-    in
-    Lwt_process.with_process_full ?env cmd f
-  with
-  | Unix.Unix_error (err, _, _) ->
-    let msg = Unix.error_message err in
-    RunAsync.error msg
-  | _ ->
-    RunAsync.error "error running subprocess"
-
-let runLifecycle ?env pkgJson install =
-  let open RunAsync.Syntax in
-  let%bind () =
-    match pkgJson.PackageJson.scripts.install with
-    | Some cmd -> runLifecycleScript ?env ~install ~lifecycleName:"install" cmd
-    | None -> return ()
-  in
-  let%bind () =
-    match pkgJson.PackageJson.scripts.postinstall with
-    | Some cmd -> runLifecycleScript ?env ~install ~lifecycleName:"postinstall" cmd
-    | None -> return ()
-  in
-  return ()
-
 let isInstalled ~(sandbox : Sandbox.t) (solution : Solution.t) =
   let open RunAsync.Syntax in
   let installationPath = SandboxSpec.installationPath sandbox.spec in
@@ -196,21 +22,6 @@ let isInstalled ~(sandbox : Sandbox.t) (solution : Solution.t) =
         return false
     in
     Solution.fold ~f ~init:(return true) solution
-
-let installBinWrapper ~binPath (name, origPath) =
-  let open RunAsync.Syntax in
-  Logs_lwt.debug (fun m ->
-    m "Fetch:installBinWrapper: %a / %s -> %a"
-    Path.pp origPath name Path.pp binPath
-  );%lwt
-  if%bind Fs.exists origPath
-  then (
-    let%bind () = Fs.chmod 0o777 origPath in
-    Fs.symlink ~src:origPath Path.(binPath / name)
-  ) else (
-    Logs_lwt.warn (fun m -> m "missing %a defined as binary" Path.pp origPath);%lwt
-    return ()
-  )
 
 let fetch ~(sandbox : Sandbox.t) (solution : Solution.t) =
   let open RunAsync.Syntax in
@@ -230,11 +41,6 @@ let fetch ~(sandbox : Sandbox.t) (solution : Solution.t) =
     return (Package.Set.remove root all, root)
   in
 
-  let directDependenciesOfRoot =
-    let deps = Solution.dependencies root solution in
-    Package.Set.of_list deps
-  in
-
   (* Fetch all package distributions *)
   let%bind dists =
     let report, finish = Cli.createProgressReporter ~name:"fetching" () in
@@ -245,31 +51,18 @@ let fetch ~(sandbox : Sandbox.t) (solution : Solution.t) =
           let status = Format.asprintf "%a" Package.pp pkg in
           report status
         in
-        let%bind dist = FetchStorage.fetch ~sandbox pkg in
-        let%bind status, path = FetchStorage.install ~sandbox dist in
-        Logs_lwt.debug (fun m ->
-          m "fetched: %a -> %a" Package.pp pkg Path.pp path);%lwt
-        let%bind pkgJson = PackageJson.ofDir path in
-        let install = {
-          Install.
-          pkg;
-          sourcePath = path;
-          path;
-          isDirectDependencyOfRoot = Package.Set.mem pkg directDependenciesOfRoot;
-          status;
-        } in
-        let id = Dist.id dist in
-        return (id, (dist, install, pkgJson))
+        FetchStorage.fetch ~sandbox pkg
       in
       RunAsync.List.mapAndJoin
         ~concurrency:8
         ~f:fetch
         (Package.Set.elements pkgs)
     in
+
     let%lwt () = finish () in
 
     let dists =
-      let f dists (id, v) = PackageId.Map.add id v dists in
+      let f dists dist = PackageId.Map.add (Dist.id dist) dist dists in
       List.fold_left ~f ~init:PackageId.Map.empty dists
     in
 
@@ -279,14 +72,8 @@ let fetch ~(sandbox : Sandbox.t) (solution : Solution.t) =
   (* Produce _esy/<sandbox>/installation.json *)
   let%bind installation =
     let installation =
-      let f id (dist, install, _pkgJson) installation =
-        let loc =
-          let source = Dist.source dist in
-          match source with
-          | Source.LocalPathLink {path; manifest = _} -> Path.(sandbox.spec.path // path)
-          | _ -> install.Install.sourcePath;
-        in
-        Installation.add id loc installation
+      let f id dist installation =
+        Installation.add id (Dist.sourcePath dist) installation
       in
       let init =
         Installation.empty
@@ -339,42 +126,6 @@ let fetch ~(sandbox : Sandbox.t) (solution : Solution.t) =
         return ()
     in
 
-    let env =
-      let override =
-        let path = (Path.show binPath)::System.Environment.path in
-        let sep = System.Environment.sep ~name:"PATH" () in
-        Astring.String.Map.(
-          empty
-          |> add "PATH" (String.concat sep path)
-        )
-      in
-      ChildProcess.CurrentEnvOverride override
-    in
-
-    let process install pkgJson =
-      let%bind () =
-        Logs_lwt.debug (fun m -> m "Fetch:runLifecycle:%a" Package.pp install.Install.pkg);%lwt
-        RunAsync.contextf
-          (runLifecycle
-            ~env
-            pkgJson
-            install)
-          "running lifecycle %a"
-          Install.pp install
-
-      in
-
-      let%bind () =
-        Logs_lwt.debug (fun m -> m "Fetch:installBinWrappers:%a" Package.pp install.Install.pkg);%lwt
-        RunAsync.List.mapAndWait
-          ~concurrency:8
-          ~f:(installBinWrapper ~binPath)
-          (PackageJson.packageCommands install.Install.sourcePath pkgJson)
-      in
-
-      return ()
-    in
-
     let seen = ref Package.Set.empty in
 
     let rec visit pkg =
@@ -398,14 +149,7 @@ let fetch ~(sandbox : Sandbox.t) (solution : Solution.t) =
         in
 
         match isRoot, PackageId.Map.find_opt (Solution.Package.id pkg) dists with
-        | false, Some (
-            _dist,
-            ({Install. status = FetchStorage.Fresh; _ } as install),
-            Some ({PackageJson. esy = None; _} as pkgJson)
-          ) ->
-          process install pkgJson
-        | false, Some (_, {status = FetchStorage.Cached;_}, _) -> return ()
-        | false, Some (_, {status = FetchStorage.Fresh;_}, _) -> return ()
+        | false, Some dist -> FetchStorage.install dist
         | false, None -> errorf "dist not found: %a" Package.pp pkg
         | true, _ -> return ()
       )
