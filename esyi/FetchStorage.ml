18139d941d67cfe36871152eddbe32b8f82f1c79
Andrey Popp <8mayday@gmail.com>
2018-10-23 19:05:41 +0300

    Wrap .js bins with shell/batch wrappers
    Fixes #533

--- esyi/FetchStorage.ml
+++ esyi/FetchStorage.ml
@@ -307,7 +307,30 @@ let runLifecycle ~binPath pkg sourcePath lifecycle =
 
   return ()
 
-let installBinWrapper ~binPath (name, origPath) =
+let installNodeBinWrapper binPath (name, origPath) =
+  let data, path =
+    match System.Platform.host with
+    | Windows ->
+      let data =
+      Format.asprintf
+        {|@ECHO off
+@SETLOCAL
+node "%a" %%*
+          |} Path.pp origPath
+      in
+      data, Path.(binPath / name |> addExt ".cmd")
+    | _ ->
+      let data =
+        Format.asprintf
+          {|#!/bin/sh
+exec node "%a" "$@"
+            |} Path.pp origPath
+      in
+      data, Path.(binPath / name)
+  in
+  Fs.writeFile ~perm:0o755 ~data path
+
+let installBinWrapper binPath (name, origPath) =
   let open RunAsync.Syntax in
   Logs_lwt.debug (fun m ->
     m "Fetch:installBinWrapper: %a / %s -> %a"
@@ -315,11 +338,15 @@ let installBinWrapper ~binPath (name, origPath) =
   );%lwt
   if%bind Fs.exists origPath
   then (
-    let%bind () = Fs.chmod 0o777 origPath in
-    let destPath = Path.(binPath / name) in
-    if%bind Fs.exists destPath
-    then return ()
-    else Fs.symlink ~src:origPath destPath
+    if Path.hasExt ".js" origPath
+    then installNodeBinWrapper binPath (name, origPath)
+    else (
+      let%bind () = Fs.chmod 0o777 origPath in
+      let destPath = Path.(binPath / name) in
+      if%bind Fs.exists destPath
+      then return ()
+      else Fs.symlink ~src:origPath destPath
+    )
   ) else (
     Logs_lwt.warn (fun m -> m "missing %a defined as binary" Path.pp origPath);%lwt
     return ()
@@ -379,7 +406,7 @@ let install dist =
       match pkgJson with
       | Some pkgJson ->
         let bin = PackageJson.bin ~sourcePath pkgJson in
-        RunAsync.List.mapAndWait ~f:(installBinWrapper ~binPath) bin
+        RunAsync.List.mapAndWait ~f:(installBinWrapper binPath) bin
       | None -> return ()
     in
 
