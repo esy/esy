0462a2f1788ce4ba6b57d8299fe2f0e62812ce5a
Bryan Phelps <bryphe@outlook.com>
2018-10-24 17:58:51 -0700

    Update install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,6 +7,8 @@ Darwin) OS=NIX;;
 *) OS=WIN;
 esac
 
+echo $OS
+
 travis_retry() {
     local result=0
     local count=1
@@ -28,17 +30,9 @@ travis_retry() {
   return $result
 }
 
-
-if [[ "$OS" -eq "NIX" ]]; then
-
+if [[ "$OS" = "NIX" ]]; then
 make bootstrap
-
 else
-
-# Bring in travis_retry to get access to the function:
-# https://gist.github.com/letmaik/caa0f6cc4375cbfcc1ff26bd4530c2a3
-source travis_retry.sh
-
 npm install -g jest-cli
 cp scripts/build/patched-bash-exec.js C:/Users/appveyor/AppData/Roaming/npm/node_modules/esy/node_modules/esy-bash/bash-exec.js
 travis_retry esy install
