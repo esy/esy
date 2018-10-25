3738050f88f4b8e2ca2cbc824a3a141e3e2ae5df
Bryan Phelps <bryphe@outlook.com>
2018-10-24 18:00:30 -0700

    Format scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,12 +8,18 @@ Darwin) OS=NIX;;
 esac
 
 if [[ "$OS_NAME" = "NIX" ]]; then
+
+    # Linux / OSX build
     make ci
     make ESY_RELEASE_TAG="$TRAVIS_TAG" build platform-release
+
 else
+
+    # Windows build
     npm run build
     jest test-e2e
     npm run test:unit
     npm run test:e2e-slow
     npm run release:make-platform-package
+
 fi
--- scripts/install.sh
+++ scripts/install.sh
@@ -29,9 +29,15 @@ travis_retry() {
 }
 
 if [[ "$OS" = "NIX" ]]; then
-make bootstrap
+
+    # Linux / OSX install
+    make bootstrap
+
 else
-npm install -g jest-cli
-cp scripts/build/patched-bash-exec.js C:/Users/appveyor/AppData/Roaming/npm/node_modules/esy/node_modules/esy-bash/bash-exec.js
-travis_retry esy install
+
+    # Windows install
+    npm install -g jest-cli
+    cp scripts/build/patched-bash-exec.js C:/Users/appveyor/AppData/Roaming/npm/node_modules/esy/node_modules/esy-bash/bash-exec.js
+    travis_retry esy install
+
 fi
