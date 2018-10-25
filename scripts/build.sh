fff3827b520d7c7695fa8524a5f4c79b5664b761
Bryan Phelps <bryphe@outlook.com>
2018-10-24 17:18:01 -0700

    Platform detection for scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 set -ev
 
-make ci
-make ESY_RELEASE_TAG="$TRAVIS_TAG" build platform-release
+OSNAME=$(uname -s)
+
+if [[ "$OS_NAME" == "Linux"]] || [[ "$OS_NAME" == "Darwin" ]]; then
+    make ci
+    make ESY_RELEASE_TAG="$TRAVIS_TAG" build platform-release
+else
+    npm run build
+    jest test-e2e
+    npm run test:unit
+    npm run test:e2e-slow
+    npm run release:make-platform-package
+fi
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 set -ev
 
+OS_NAME=$(uname -s)
+
+if [[ "$OS_NAME" == "Linux"]] || [[ "$OS_NAME" == "Darwin" ]]; then
 make bootstrap
+else
+npm install -g jest-cli
+cp scripts/build/patched-bash-exec.js C:/Users/appveyor/AppData/Roaming/npm/node_modules/esy/node_modules/esy-bash/bash-exec.js
+travis_retry esy install
+fi
