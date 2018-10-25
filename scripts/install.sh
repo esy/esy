508964548fbafc573aadc205e7ef9ef2608b5ebe
Bryan Phelps <bryphe@outlook.com>
2018-10-24 17:56:31 -0700

    Tweak install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,38 @@
 #!/bin/bash
 set -e
 
-OS_NAME="$(uname -s)"
+case $(uname -s) in
+Linux) OS=NIX;;
+Darwin) OS=NIX;;
+*) OS=WIN;
+esac
 
-if [[ "$OS_NAME" -eq "Linux" ]] || [["$OS_NAME" -eq "Darwin" ]]; then
+travis_retry() {
+    local result=0
+    local count=1
+    while [ $count -le 3 ]; do
+        [ $result -ne 0 ] && {
+        echo -e "\n${ANSI_RED}The command \"$@\" failed. Retrying, $count of 3.${ANSI_RESET}\n" >&2
+    }
+    # ! { } ignores set -e, see https://stackoverflow.com/a/4073372
+    ! { "$@"; result=$?; }
+    [ $result -eq 0 ] && break
+    count=$(($count + 1))
+    sleep 1
+    done
+
+    [ $count -gt 3 ] && {
+    echo -e "\n${ANSI_RED}The command \"$@\" failed 3 times.${ANSI_RESET}\n" >&2
+  }
+
+  return $result
+}
+
+
+if [[ "$OS" -eq "NIX" ]]; then
+
 make bootstrap
+
 else
 
 # Bring in travis_retry to get access to the function:
