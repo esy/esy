0c893e23b3321db0d69baa5beba663baa508a4ce
Bryan Phelps <bryphe@outlook.com>
2018-10-24 18:11:31 -0700

    Fix

--- scripts/install.sh
+++ scripts/install.sh
@@ -37,7 +37,8 @@ else
 
     # Windows install
     npm install -g jest-cli
-    cp scripts/build/patched-bash-exec.js C:/Users/appveyor/AppData/Roaming/npm/node_modules/esy/node_modules/esy-bash/bash-exec.js
+    which esy
+    # cp scripts/build/patched-bash-exec.js /c/Users/appveyor/AppData/Roaming/npm/node_modules/esy/node_modules/esy-bash/bash-exec.js
     travis_retry esy install
 
 fi
