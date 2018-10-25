448cfd1b5847c6b64a97f1f9cb6dfdf032bb7757
Bryan Phelps <bryphe@outlook.com>
2018-10-24 18:26:15 -0700

    See if esy is available in powershell env

--- scripts/install.sh
+++ scripts/install.sh
@@ -37,7 +37,7 @@ else
 
     # Windows install
     npm install -g jest-cli
-    which esy
+    powershell which esy
     # cp scripts/build/patched-bash-exec.js /c/Users/appveyor/AppData/Roaming/npm/node_modules/esy/node_modules/esy-bash/bash-exec.js
     travis_retry esy install
 
