f07d45da977a38a722cd64c5d799dfa74b17778c
Bryan Phelps <bryphe@outlook.com>
2018-10-24 17:20:42 -0700

    Source travis retry

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,6 +6,7 @@ OS_NAME=$(uname -s)
 if [[ "$OS_NAME" == "Linux"]] || [[ "$OS_NAME" == "Darwin" ]]; then
 make bootstrap
 else
+source travis_retry.sh
 npm install -g jest-cli
 cp scripts/build/patched-bash-exec.js C:/Users/appveyor/AppData/Roaming/npm/node_modules/esy/node_modules/esy-bash/bash-exec.js
 travis_retry esy install
