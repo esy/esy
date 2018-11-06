52606f09f2ee562f5f7cdaa4f0bff4f575b6d37a
Andrey Popp <8mayday@gmail.com>
2018-11-06 17:11:11 +0300

    Remove OCAMLFIND_COMMANDS from build env (#595)
    It was added long ago and I'm not sure why.
    
    By default `bin/ocaml*` are symlinked to `bin/ocaml*.opt` if native
    toolchain is installed so I don't think we need it now.
    
    Also on Windows there warning related to this env variable, see #594.

--- test-e2e/common/command-env.test.js
+++ test-e2e/common/command-env.test.js
@@ -63,7 +63,6 @@ describe('esy command-env', () => {
     expect(env.OCAMLPATH).toBeTruthy();
     expect(env.OCAMLFIND_LDCONF).toBeTruthy();
     expect(env.OCAMLFIND_DESTDIR).toBeTruthy();
-    expect(env.OCAMLFIND_COMMANDS).toBeTruthy();
     expect(env.MAN_PATH).toBeTruthy();
     expect(env.CAML_LD_LIBRARY_PATH).toBeTruthy();
 
