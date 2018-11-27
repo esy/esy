2c9bdec799c14ad578c783eb0313354ca1d7286b
Andrey Popp <8mayday@gmail.com>
2018-11-27 16:24:30 +0300

    offline.test: add test case which checks for cached tarballs

--- test-e2e/install/offline.test.js
+++ test-e2e/install/offline.test.js
@@ -46,4 +46,49 @@ describe('offline installation workflow', function() {
     // this should work as we specified a path to tarballs
     await p.esy(`fetch --cache-tarballs-path ${tarballsPath}`);
   });
+
+  test('it can checks that cached tarballs are downloaded', async function() {
+    const p = await helpers.createTestSandbox();
+
+    await p.fixture(
+      helpers.packageJson({
+        name: 'root',
+        dependencies: {
+          a: '*',
+          b: '*',
+        },
+      }),
+    );
+
+    await p.defineNpmPackage({
+      name: 'a',
+      version: '1.0.0',
+    });
+
+    await p.defineNpmPackage({
+      name: 'b',
+      version: '2.0.0',
+    });
+
+    const tarballsPath = path.join(p.projectPath, 'sources');
+
+    // perform install so that we know sources are cached already
+    await p.esy(`install`);
+
+    await p.esy(`install --cache-tarballs-path ${tarballsPath}`);
+    await fs.exists(tarballsPath);
+
+    // remove caches
+    await fs.remove(path.join(p.projectPath, '_esy'));
+    await fs.remove(path.join(p.esyPrefixPath, 'esyi', 'source'));
+
+    // shutdown npm registry so just fetch would fail
+    await p.npmRegistry.shutdown();
+
+    // this won't succeed as we stopped npm registry
+    await expect(p.esy(`fetch`)).rejects.toThrow();
+
+    // this should work as we specified a path to tarballs
+    await p.esy(`fetch --cache-tarballs-path ${tarballsPath}`);
+  });
 });
