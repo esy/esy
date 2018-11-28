58886b1998117548a42ce0e7b3183fb6965b638e
Andrey Popp <8mayday@gmail.com>
2018-11-28 19:00:17 +0300

    build-anycmd.test: cleanup & add case for linked packages

--- test-e2e/common/build-anycmd.test.js
+++ test-e2e/common/build-anycmd.test.js
@@ -3,33 +3,112 @@
 const os = require('os');
 const path = require('path');
 
-const {createTestSandbox, skipSuiteOnWindows} = require('../test/helpers');
+const helpers = require('../test/helpers');
 const fixture = require('./fixture.js');
+const {packageJson, dir, file, dummyExecutable, buildCommand} = helpers;
 
-skipSuiteOnWindows();
+helpers.skipSuiteOnWindows();
 
-it('Common - build anycmd', async () => {
-  const p = await createTestSandbox();
-  await p.fixture(...fixture.makeSimpleProject(p));
+describe(`'esy build CMD' invocation`, () => {
+  function createPackage(p, name) {
+    return dir(
+      name,
+      packageJson({
+        name,
+        version: '1.0.0',
+        esy: {
+          install: [
+            'cp #{self.root / self.name}.js #{self.bin / self.name}.js',
+            buildCommand(p, '#{self.bin / self.name}.js'),
+          ],
+        },
+      }),
 
-  await p.esy('install');
-  await p.esy('build');
+      dummyExecutable(name),
+    );
+  }
 
-  await expect(p.esy('build dep.cmd')).resolves.toEqual({
-    stdout: '__dep__' + os.EOL,
-    stderr: '',
+  async function createTestSandbox() {
+    const p = await helpers.createTestSandbox();
+    await p.fixture(
+      packageJson({
+        name: 'simple-project',
+        version: '1.0.0',
+        dependencies: {
+          dep: 'path:./dep',
+          linkedDep: '*',
+        },
+        devDependencies: {
+          devDep: 'path:./devDep',
+        },
+        resolutions: {
+          linkedDep: 'link:./linkedDep',
+        },
+      }),
+      createPackage(p, 'dep'),
+      createPackage(p, 'linkedDep'),
+      createPackage(p, 'devDep'),
+    );
+    return p;
+  }
+
+  it(`invokes commands in build environment`, async () => {
+    const p = await createTestSandbox();
+    await p.esy('install');
+    await p.esy('build');
+
+    await expect(p.esy('build dep.cmd')).resolves.toEqual({
+      stdout: '__dep__' + os.EOL,
+      stderr: '',
+    });
   });
 
-  await expect(p.esy('b dep.cmd')).resolves.toEqual({
-    stdout: '__dep__' + os.EOL,
-    stderr: '',
+  it(`cannot invoke commands defined in devDependencies`, async () => {
+    const p = await createTestSandbox();
+    await p.esy('install');
+    await p.esy('build');
+
+    await expect(p.esy('build devDep.cmd')).rejects.toMatchObject({
+      code: 1,
+    });
   });
 
-  // make sure exit code is preserved
-  await expect(p.esy("b bash -c 'exit 1'")).rejects.toEqual(
-    expect.objectContaining({code: 1}),
-  );
-  await expect(p.esy("b bash -c 'exit 7'")).rejects.toEqual(
-    expect.objectContaining({code: 7}),
-  );
+  it(`builds dependencies before running a command`, async () => {
+    const p = await createTestSandbox();
+    await p.esy('install');
+
+    await p.esy('build dep.cmd');
+  });
+
+  it(`builds linked dependencies before running a command`, async () => {
+    const p = await createTestSandbox();
+    await p.esy('install');
+
+    await p.esy('build linkedDep.cmd');
+  });
+
+  it(`has 'esy b CMD' shortcut`, async () => {
+    const p = await createTestSandbox();
+    await p.esy('install');
+    await p.esy('build');
+
+    await expect(p.esy('b dep.cmd')).resolves.toEqual({
+      stdout: '__dep__' + os.EOL,
+      stderr: '',
+    });
+  });
+
+  it(`preserves exit code of the command it runs`, async () => {
+    const p = await createTestSandbox();
+    await p.esy('install');
+    await p.esy('build');
+
+    // make sure exit code is preserved
+    await expect(p.esy("b bash -c 'exit 1'")).rejects.toEqual(
+      expect.objectContaining({code: 1}),
+    );
+    await expect(p.esy("b bash -c 'exit 7'")).rejects.toEqual(
+      expect.objectContaining({code: 7}),
+    );
+  });
 });
