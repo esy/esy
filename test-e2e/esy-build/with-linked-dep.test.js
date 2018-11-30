60c307d75f32dadf7927a75935948743208f70d0
Andrey Popp <8mayday@gmail.com>
2018-11-30 14:36:09 +0300

    Followup fix for normalizing build id for dep

--- test-e2e/esy-build/with-linked-dep.test.js
+++ test-e2e/esy-build/with-linked-dep.test.js
@@ -82,8 +82,9 @@ describe('Build with a linked dep', () => {
       'macos || linux: build-env snapshot',
       withProject(async function(p) {
         const id = JSON.parse((await p.esy('build-plan')).stdout).id;
+        const depid = JSON.parse((await p.esy('build-plan dep')).stdout).id;
         const {stdout} = await p.esy('build-env');
-        expect(p.normalizePathsForSnapshot(stdout, {id})).toMatchSnapshot();
+        expect(p.normalizePathsForSnapshot(stdout, {id, depid})).toMatchSnapshot();
       }),
     );
 
