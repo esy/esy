2b7bd80fe68c609fb2b99035a284e6d91422ccdf
Bryan Phelps <bryphe@outlook.com>
2018-12-10 21:36:33 -0800

    Try setting long path enabled registry key

--- SetLongPathRegistry.ps1
+++
@@ -1,3 +0,0 @@
-Test-Path HKLM:System\CurrentControlSet\Control\FileSystem
-Get-ItemProperty -Path HKLM:System\CurrentControlSet\Control\FileSystem
-Set-ItemProperty -Path HKLM:System\CurrentControlSet\Control\FileSystem -Value 1
