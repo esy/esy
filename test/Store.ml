532c5c9d653b921aaa845cfcb76517adaa236d86
Bryan Phelps <bryphe@outlook.com>
2018-12-18 16:36:33 -0800

    Update unit tests

--- test/Store.ml
+++ test/Store.ml
@@ -2,12 +2,20 @@ include EsyLib.Store
 
 module System = EsyLib.System
 
-let%test "Validate padding length on Windows is always 1" = 
+let%test "Validate padding length on Windows is 1, if long paths aren't supported" = 
     let prefixPath = Fpath.v "test" in
-    let padding = getPadding ~system:System.Platform.Windows prefixPath in
+    let padding = getPadding ~system:System.Platform.Windows ~longPaths:false prefixPath in
     match padding with 
         | Ok("_") -> true
         | _ -> false
+
+let%test "Validate padding length on Windows is not 1, if long paths are supported" = 
+    let prefixPath = Fpath.v "test" in
+    let padding = getPadding ~system:System.Platform.Windows ~longPaths:true prefixPath in
+    match padding with 
+        | Ok("_") -> false
+        | Error(_) -> false
+        | _ -> true
 
 let%test "Validate padding length on other platforms is not 1" =
     let prefixPath = Fpath.v "test" in
