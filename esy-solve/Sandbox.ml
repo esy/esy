5b18a3861271586e13fdaf16437e7d032b0994b9
Andrey Popp <8mayday@gmail.com>
2018-12-21 18:12:13 +0300

    EsySolve.Sandbox: construct sandbox for *.opam by linking
    Instead of creating a single big package with all metadata aggregated we
    constructs multiple packages and depend on them from a synthesized root:
    
        *.opam -> one.opam
               -> another.opam
    
    This is much more correct as those packages will have their own scope.
    Consider `*.opam` being a monorepo.

--- esy-solve/Sandbox.ml
+++ esy-solve/Sandbox.ml
@@ -1,3 +1,7 @@
+module Version = EsyInstall.Version
+module Source = EsyInstall.Source
+module PackageConfig = EsyInstall.PackageConfig
+
 type t = {
   cfg : Config.t;
   spec : EsyInstall.SandboxSpec.t;
@@ -8,161 +12,16 @@ type t = {
   resolver : Resolver.t;
 }
 
-let ocamlReqAny =
-  let spec = EsyInstall.VersionSpec.Npm EsyInstall.SemverVersion.Formula.any in
-  EsyInstall.Req.make ~name:"ocaml" ~spec
+let makeResolution source = {
+  EsyInstall.PackageConfig.Resolution.
+  name = "root";
+  resolution = Version (Version.Source source);
+}
 
-let ofMultiplOpamFiles ~cfg ~spec _projectPath (paths : Path.t list) =
+let ofResolution cfg spec resolver resolution =
   let open RunAsync.Syntax in
-
-  let%bind resolver = Resolver.make ~cfg ~sandbox:spec () in
-
-  let%bind opams =
-
-    let readOpam (path : Path.t) =
-      let%bind data = Fs.readFile path in
-      if String.trim data = ""
-      then return None
-      else
-        let name = Path.(path |> remExt |> basename) in
-        let%bind manifest =
-          let version = OpamPackage.Version.of_string "dev" in
-          OpamManifest.ofPath ~name:(OpamPackage.Name.of_string name) ~version path
-        in
-        return (Some (name, manifest, path))
-    in
-
-    let%bind opams =
-      paths
-      |> List.map ~f:readOpam
-      |> RunAsync.List.joinAll
-    in
-
-    return (List.filterNone opams)
-  in
-
-  let namesPresent =
-    let f names (name, _, _) = StringSet.add ("@opam/" ^ name) names in
-    List.fold_left ~f ~init:StringSet.empty opams
-  in
-
-  let filterFormulaWithNamesPresent (deps : Package.Dep.t list list) =
-    let filterDep (dep : Package.Dep.t) = not (StringSet.mem dep.name namesPresent) in
-    let filterDisj deps =
-      match List.filter ~f:filterDep deps with
-      | [] -> None
-      | f -> Some f
-    in
-    deps
-    |> List.map ~f:filterDisj
-    |> List.filterNone
-  in
-
-  let source = EsyInstall.Source.Link {
-    path = EsyInstall.DistPath.v ".";
-    manifest = None;
-  } in
-  let version = EsyInstall.Version.Source source in
-
-  match opams with
-  | [] ->
-    let dependencies = Package.Dependencies.NpmFormula [] in
-    return {
-      cfg;
-      spec;
-      root = {
-        name = "empty";
-        version;
-        originalVersion = None;
-        originalName = None;
-        source = EsyInstall.PackageSource.Link {
-          path = EsyInstall.DistPath.v ".";
-          manifest = None;
-        };
-        overrides = EsyInstall.Overrides.empty;
-        dependencies;
-        devDependencies = dependencies;
-        peerDependencies = EsyInstall.PackageConfig.NpmFormula.empty;
-        optDependencies = StringSet.empty;
-        resolutions = EsyInstall.PackageConfig.Resolutions.empty;
-        kind = Esy;
-      };
-      resolver;
-      resolutions = EsyInstall.PackageConfig.Resolutions.empty;
-      dependencies = Package.Dependencies.NpmFormula [];
-      ocamlReq = Some ocamlReqAny;
-    }
-  | opams ->
-    let%bind pkgs =
-      let f (name, opam, _) =
-        match%bind OpamManifest.toPackage ~name ~version opam with
-        | Ok pkg -> return pkg
-        | Error err -> error err
-      in
-      RunAsync.List.joinAll (List.map ~f opams)
-    in
-    let dependencies, devDependencies =
-      let f (dependencies, devDependencies) (pkg : Package.t) =
-        match pkg.dependencies, pkg.devDependencies with
-        | Package.Dependencies.OpamFormula du, Package.Dependencies.OpamFormula ddu ->
-          (filterFormulaWithNamesPresent du) @ dependencies,
-          (filterFormulaWithNamesPresent ddu) @ devDependencies
-        | _ -> assert false
-      in
-      List.fold_left ~f ~init:([], []) pkgs
-    in
-    let root = {
-      Package.
-      name = "root";
-      version;
-      originalVersion = None;
-      originalName = None;
-      source = EsyInstall.PackageSource.Link {
-        path = EsyInstall.DistPath.v ".";
-        manifest = None;
-      };
-      overrides = EsyInstall.Overrides.empty;
-      dependencies = Package.Dependencies.OpamFormula dependencies;
-      devDependencies = Package.Dependencies.OpamFormula devDependencies;
-      peerDependencies = EsyInstall.PackageConfig.NpmFormula.empty;
-      optDependencies = StringSet.empty;
-      resolutions = EsyInstall.PackageConfig.Resolutions.empty;
-      kind = Package.Esy;
-    } in
-
-    let dependencies =
-      Package.Dependencies.OpamFormula (dependencies @ devDependencies)
-    in
-
-    return {
-      cfg;
-      spec;
-      root;
-      resolutions = EsyInstall.PackageConfig.Resolutions.empty;
-      resolver;
-      dependencies;
-      ocamlReq = Some ocamlReqAny;
-    }
-
-let ofSource ~cfg ~spec source =
-  let open RunAsync.Syntax in
-
-  let%bind resolution =
-    let version = EsyInstall.Version.Source source in
-    return {
-      EsyInstall.PackageConfig.Resolution.
-      name = "root";
-      resolution = Version version;
-    }
-  in
-
-  let%bind resolver =
-    Resolver.make ~cfg ~sandbox:spec ()
-  in
-
   match%bind Resolver.package ~resolution resolver with
   | Ok root ->
-
     let root =
       let name =
         match root.Package.originalName with
@@ -186,8 +45,6 @@ let ofSource ~cfg ~spec source =
         failwith "mixing npm and opam dependencies"
     in
 
-    Resolver.setResolutions root.resolutions resolver;
-
     return {
       cfg;
       spec;
@@ -199,17 +56,76 @@ let ofSource ~cfg ~spec source =
     }
   | Error msg -> errorf "unable to construct sandbox: %s" msg
 
+let anyOpam = EsyInstall.VersionSpec.Opam (EsyInstall.OpamPackageVersion.Formula.any)
+
 let make ~cfg (spec : EsyInstall.SandboxSpec.t) =
+  let open RunAsync.Syntax in
+  let path = EsyInstall.DistPath.make ~base:spec.path spec.path in
+  let makeSource manifest =
+    Source.Link {path; manifest = Some manifest;}
+  in
   RunAsync.contextf (
+    let%bind resolver = Resolver.make ~cfg ~sandbox:spec () in
     match spec.manifest with
-    | EsyInstall.SandboxSpec.Manifest (Esy, fname)
-    | EsyInstall.SandboxSpec.Manifest (Opam, fname) ->
-      let source = "link:" ^ fname in
-      begin match EsyInstall.Source.parse source with
-      | Ok source -> ofSource ~cfg ~spec source
-      | Error msg -> RunAsync.errorf "unable to construct sandbox: %s" msg
-      end
-    | EsyInstall.SandboxSpec.ManifestAggregate _ ->
-      let paths = EsyInstall.SandboxSpec.manifestPaths spec in
-      ofMultiplOpamFiles ~cfg ~spec spec.path paths
+    | EsyInstall.SandboxSpec.Manifest manifest ->
+      let source = makeSource manifest in
+      let resolution = makeResolution source in
+      let%bind sandbox = ofResolution cfg spec resolver resolution in
+      Resolver.setResolutions sandbox.resolutions sandbox.resolver;
+      return sandbox
+    | EsyInstall.SandboxSpec.ManifestAggregate manifests ->
+      let%bind resolutions, reqs, devDeps =
+        let f (resolutions, reqs, devDeps) manifest  =
+          let source = makeSource manifest in
+          let resolution = makeResolution source in
+          match%bind Resolver.package ~resolution resolver with
+          | Error msg -> errorf "unable to read %a: %s" EsyInstall.ManifestSpec.pp manifest msg
+          | Ok pkg ->
+            let name =
+              match EsyInstall.ManifestSpec.inferPackageName manifest with
+              | None -> failwith "TODO"
+              | Some name -> name
+            in
+            let resolutions =
+              let resolution = PackageConfig.Resolution.Version (EsyInstall.Version.Source source) in
+              PackageConfig.Resolutions.add name resolution resolutions
+            in
+            let reqs = (EsyInstall.Req.make ~name ~spec:anyOpam)::reqs in
+            let devDeps =
+              match pkg.Package.devDependencies with
+              | Package.Dependencies.OpamFormula deps -> deps @ devDeps
+              | Package.Dependencies.NpmFormula _ -> devDeps
+            in
+            return (resolutions, reqs, devDeps)
+        in
+        RunAsync.List.foldLeft ~f ~init:(PackageConfig.Resolutions.empty, [], []) manifests
+      in
+      Resolver.setResolutions resolutions resolver;
+      let root = {
+        Package.
+        name = Path.basename spec.path;
+        version = EsyInstall.Version.Source (Dist NoSource);
+        originalVersion = None;
+        originalName = None;
+        source = EsyInstall.PackageSource.Install {
+          source = NoSource, [];
+          opam = None;
+        };
+        overrides = EsyInstall.Overrides.empty;
+        dependencies = Package.Dependencies.NpmFormula reqs;
+        devDependencies = Package.Dependencies.OpamFormula devDeps;
+        peerDependencies = EsyInstall.PackageConfig.NpmFormula.empty;
+        optDependencies = StringSet.empty;
+        resolutions;
+        kind = Npm;
+      } in
+      return {
+        cfg;
+        spec;
+        root;
+        resolutions = root.resolutions;
+        ocamlReq = None;
+        dependencies = Package.Dependencies.NpmFormula reqs;
+        resolver;
+      }
   ) "loading root package metadata"
