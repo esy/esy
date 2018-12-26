e986cf3f184202bc3cbe38deff7fee02a4463128
Andrey Popp <8mayday@gmail.com>
2018-12-26 14:43:13 +0300

    EnvSpec: prettier docstrings

--- esy/EnvSpec.mli
+++ esy/EnvSpec.mli
@@ -1,14 +1,29 @@
 (** This describes how to construct environment for command invocations. *)
 
 type t = {
+  (**
+    Defines what packages we should bring into the command env.
+    *)
   augmentDeps : DepSpec.t option;
-  (** Defines what packages we should bring into the command env. *)
+
+  (**
+    If we should init the build environment (enable sandboxing, do source
+    relloc).
+    *)
   buildIsInProgress : bool;
-  (** If we should init the build environment (enable sandboxing, do source relloc). *)
+
+  (**
+    If we should include current environment.
+    *)
   includeCurrentEnv : bool;
-  (** If we should include current environment. *)
+
+  (**
+    If we should include the package's build environment.
+    *)
   includeBuildEnv : bool;
-  (** If we should include the package's build environment. *)
+
+  (**
+    If we should include the project's npm bin in $PATH.
+    *)
   includeNpmBin : bool;
-  (** If we should include the project's npm bin in $PATH. *)
 }
