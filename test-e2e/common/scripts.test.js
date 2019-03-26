e3972c7743da5e2188b2e403f9da9128fffb7772
Oleksiy Golovko <alexei.golovko@gmail.com>
2019-03-26 16:19:21 +0100

    add `run-script` command

--- test-e2e/common/scripts.test.js
+++ test-e2e/common/scripts.test.js
@@ -30,7 +30,7 @@ const fixture = [
       build_cmd4: "esy build bash -c 'echo 'build_cmd_result''",
       build_cmd5: 'esy build echo #{self.name}',
       build_cmd6: [['esy', 'build', 'echo', '#{self.name}']],
-      printpwd: "pwd",
+      printpwd: 'pwd',
     },
     esy: {
       build: [
@@ -40,11 +40,11 @@ const fixture = [
       install: ['cp $cur__target_dir/script $cur__bin/script'],
     },
     dependencies: {
-      dep: '*'
+      dep: '*',
     },
     resolutions: {
-      dep: 'link:./dep'
-    }
+      dep: 'link:./dep',
+    },
   }),
   file(
     'script.sh',
@@ -54,16 +54,17 @@ const fixture = [
     echo 'script_exec_result'
   `,
   ),
-  dir('dep',
+  dir(
+    'dep',
     packageJson({
       name: 'dep',
       version: '1.0.0',
       esy: {
-        build: 'true'
-      }
-    })
+        build: 'true',
+      },
+    }),
   ),
-  dir('foo')
+  dir('foo'),
 ];
 
 it('executes scripts', async () => {
@@ -138,17 +139,34 @@ it('does execute scripts in a non-root package scope', async () => {
   await p.esy('build');
 
   await expect(p.esy('-p dep cmd1')).rejects.toMatchObject({
-    message: expect.stringMatching('error: unable to resolve command: cmd1')
+    message: expect.stringMatching('error: unable to resolve command: cmd1'),
   });
 });
 
-it('executes script in the package\'s root', async () => {
+it("executes script in the package's root", async () => {
   const p = await createTestSandbox(...fixture);
   await p.esy('install');
   await p.esy('build');
-  p.cd("foo");
+  p.cd('foo');
   const cmd = await p.esy('printpwd');
-  const splitPath = cmd.stdout.split("/");
+  const splitPath = cmd.stdout.split('/');
   const folderName = splitPath[splitPath.length - 1];
   expect(folderName).toEqual(expect.not.stringMatching(/foo/));
-})
+});
+
+it('run-script executes the specified script', async () => {
+  const p = await createTestSandbox(...fixture);
+  await p.esy('install');
+  const result = await p.esy('run-script printpwd');
+  expect(result.stderr).toEqual('');
+  expect(result.stdout).toMatch(/^\//); // TODO: handle Windows
+});
+
+it('run-script fails when script is not found', async () => {
+  const p = await createTestSandbox(...fixture);
+  await p.esy('install');
+
+  await expect(p.esy('run-script non-existent')).rejects.toMatchObject({
+    message: expect.stringMatching("error: Script 'non-existent' not found"),
+  });
+});
