e22e56a3cdbeecb841bd28757e648c4572e1c185
Oleksiy Golovko <alexei.golovko@gmail.com>
2019-04-03 14:28:33 +0200

    add test dumping the CUDF input/output to file

--- test-e2e/common/solve.test.js
+++ test-e2e/common/solve.test.js
@@ -1,3 +1,5 @@
+const path = require('path');
+const fs = require('fs-extra');
 const outdent = require('outdent');
 const helpers = require('../test/helpers.js');
 const {packageJson, file, dir} = helpers;
@@ -33,6 +35,57 @@ describe('esy solve', function() {
 
     request: 
     install: root = 1
+    preamble: 
+    property: staleness: int, original-version: string
+
+    package: root
+    version: 1
+    conflicts: root
+    installed: true
+    original-version: 0.0.0
+    staleness: 0
+    `);
+  });
+
+  it('dumps CUDF input & output to files on disk', async () => {
+    const p = await helpers.createTestSandbox();
+
+    await p.fixture(
+      packageJson({
+        name: 'root',
+        version: '1.0.0',
+        esy: {},
+        dependencies: {},
+      }),
+    );
+
+    const res = await p.esy(
+      'solve --dump-cudf-input=cudf.in --dump-cudf-output=cudf.out --skip-repository-update',
+    );
+
+    const cudfIn = fs
+      .readFileSync(path.join(p.projectPath, 'cudf.in'))
+      .toString()
+      .trim();
+    expect(cudfIn).toEqual(outdent`
+    preamble: 
+    property: staleness: int, original-version: string
+
+    package: root
+    version: 1
+    conflicts: root
+    staleness: 0
+    original-version: 0.0.0
+
+    request: 
+    install: root = 1
+    `);
+
+    const cudfOut = fs
+      .readFileSync(path.join(p.projectPath, 'cudf.out'))
+      .toString()
+      .trim();
+    expect(cudfOut).toEqual(outdent`
     preamble: 
     property: staleness: int, original-version: string
 
