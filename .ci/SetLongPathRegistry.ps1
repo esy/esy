98ce2e7b7602021731ad7f4d5e3b9814283f8dca
Ulrik <ulrik.strid@outlook.com>
2019-08-20 13:32:57 +0200

    Disable short paths in CI

--- .ci/SetLongPathRegistry.ps1
+++ .ci/SetLongPathRegistry.ps1
@@ -2,7 +2,7 @@ Test-Path HKLM:System\CurrentControlSet\Control\FileSystem
 Write-Host Getting long path registry value...
 Get-ItemProperty -Path HKLM:System\CurrentControlSet\Control\FileSystem -Name LongPathsEnabled
 Write-Host Setting long path registry value...
-Set-ItemProperty -Path HKLM:System\CurrentControlSet\Control\FileSystem -Name LongPathsEnabled -Value 1
+Set-ItemProperty -Path HKLM:System\CurrentControlSet\Control\FileSystem -Name LongPathsEnabled -Value 0
 Write-Host New Value
 Get-ItemProperty -Path HKLM:System\CurrentControlSet\Control\FileSystem -Name LongPathsEnabled
 Write-Host DONE $LastExitCode
