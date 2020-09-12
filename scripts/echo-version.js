21fb83fce8f472d31738b6768c79372955ec29ac
Manas <3097018+ManasJayanth@users.noreply.github.com>
2020-09-13 01:53:51 +0530

    Adds Github action to log sha checksums

---
+++ scripts/echo-version.js
@@ -0,0 +1,3 @@
+const packageJson = require('../package.json');
+
+console.log(packageJson.version);
---
+++ scripts/subst-release-version.js
@@ -0,0 +1,10 @@
+const fs = require ('fs');
+const path = require ('path');
+let {name, version} = require ("../ package.json");
+
+
+let pkgJson = path.join (process.cwd (), 'release', 'package.json');
+const pkgJsonData = JSON.parse (fs.readFileSync (pkgJson, 'utf8'));
+pkgJsonData.name = name;
+pkgJsonData.version = version;
+fs.writeFileSync (pkgJson, JSON.stringify (pkgJsonData, null, 2));
