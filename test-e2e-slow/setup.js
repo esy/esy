972f8733b1d4b38ddd23df41305649bae8957a4a
Manas <3097018+ManasJayanth@users.noreply.github.com>
2021-01-28 21:46:20 +0530

    @slowtest Fix e2e tests to reflect new commands layout

--- test-e2e-slow/setup.js
+++ test-e2e-slow/setup.js
@@ -97,7 +97,7 @@ function createSandbox() /* : TestSandbox */ {
     console.log(`EXEC: ${cmd}`);
     childProcess.execSync(cmd, {
       cwd: cwd,
-      env: {...process.env, ESY__PREFIX: esyPrefixPath},
+      env: {...process.env, ESY__PREFIX: esyPrefixPath, "_": cmd.split(' ')[0]},
       stdio: 'inherit',
     });
   }
