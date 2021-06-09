41e433a71694cf45f5ca2e22bfd96677380d1d31
Melwyn Saldanha <melwyn95@gmail.com>
2021-06-09 11:12:52 +0530

    remove build bash script

--- bin/build.sh
+++
@@ -1,8 +0,0 @@
-esy
-esy release
-cd _release
-npm pack
-npm install --prefix /tmp/esy esy-0.6.10.tgz
-# export PATH="/tmp/esy/node_modules/esy/bin:$PATH"
-# export PATH="/tmp/esy/node_modules/esy/bin:$PATH"
-# Add a dune target when build 
