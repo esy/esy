80938217c59f58d489098c0aacae94717a43e98a
Manas Jayanth <3097018+ManasJayanth@users.noreply.github.com>
2023-10-30 16:24:57 +0530

    Fixes -lz: quoting issues

--- boot/build-env.sh
+++ boot/build-env.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
+set -x
 
 ENV_FILE="$1"
 PATH_FILE="$2"
 CMD="$3"
 
-env -i -P $(cat "$PATH_FILE") -S $(cat "$ENV_FILE") bash -c "$CMD"
+S_VAL="$(cat "$ENV_FILE") ${CMD}"
+env -i -P $(cat "$PATH_FILE") -S "${S_VAL}"
