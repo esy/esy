eb049686c5f1a1a9c39ab2ce9b984d443fa928c9
Manas Jayanth <3097018+ManasJayanth@users.noreply.github.com>
2023-12-05 18:27:25 +0530

    test-e2e: copy binary artifacts since esy b uses symlinks

--- jest.config.js
+++ jest.config.js
@@ -1,26 +1,40 @@
 var isCi = require('is-ci');
 var path = require('path');
-
+var fs = require('fs');
 var reporters = ['default'];
 
 // Perhaps a bug in Jest - but resolution fails if
 // we give it ["jest-junit"], so we'll resolve
 // the complete path directly and pass it in.
-let junitPath  = require.resolve("jest-junit");
+let junitPath = require.resolve('jest-junit');
 const isWindows = process.platform === 'win32';
 
 if (isCi) {
   reporters = reporters.concat([junitPath]);
 }
 
-var __ESY__base = path.join(__dirname, '_build', 'install', 'default', 'bin');
-var __ESY__ = path.join(__ESY__base, 'esy');
-
-if (isWindows) {
-  __ESY__ += ".exe";
+var __ESY__;
+if (process.platform !== 'linux') {
+  var __ESY__base = path.join(__dirname, '_build', 'install', 'default', 'bin');
+  __ESY__ = path.join(__ESY__base, 'esy');
+} else {
+  const testEsyPrefix = path.join(__dirname, '.test-esy');
+  if (process.platform === 'linux') {
+    fs.rmSync(testEsyPrefix, {force: true, recursive: true});
+    fs.mkdirSync(testEsyPrefix, {recursive: true});
+    fs.cpSync(path.join(__dirname, '_build', 'install', 'default'), testEsyPrefix, {
+      recursive: true,
+      dereference: true,
+    });
+  }
+  __ESY__ = path.join(testEsyPrefix, 'bin', 'esy');
 }
 
-process.env.PATH = __ESY__base + (isWindows ? ';': ':') + process.env.PATH;
+if (isWindows) {
+  __ESY__ += '.exe';
+}
+
+process.env.PATH = __ESY__base + (isWindows ? ';' : ':') + process.env.PATH;
 
 module.exports = {
   displayName: 'e2e:fast',
@@ -34,5 +48,5 @@ module.exports = {
   coverageReporters: ['text-summary', 'json', 'html', 'cobertura'],
   reporters: reporters,
   collectCoverage: isCi,
-  globals: {__ESY__: __ESY__}
+  globals: {__ESY__: __ESY__},
 };
